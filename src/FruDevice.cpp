3f28654df4a4e4373a77ad7d5dff203f22c7c5d7
James Feist <james.feist@linux.intel.com>
2019-03-07 13:57:36 -0800

    fru-device: add extra debug print
    Let the user known when the header checksum is invalid.
    
    Change-Id: I6dc97e88987819291197b5c824f5d1fcbc9348ec
    Signed-off-by: James Feist <james.feist@linux.intel.com>

--- src/FruDevice.cpp
+++ src/FruDevice.cpp
@@ -209,6 +209,12 @@ int get_bus_frus(int file, int first, int last, int bus,
                 }
                 devices->emplace(ii, device);
             }
+            else
+            {
+                std::cerr << "Illegal header checksum at bus " << bus
+                          << " address " << ii << "\n";
+                return -1;
+            }
         }
         return 1;
     });
