75966574a966e46a707f0fb8a0112af9557b0f46
Patrick Venture <venture@google.com>
2019-08-13 09:28:08 -0700

    devices: add 24c02
    Signed-off-by: Patrick Venture <venture@google.com>
    Change-Id: Ib84fab69f794cdbb807e177ba61602276d6081d4

--- include/devices.hpp
+++ include/devices.hpp
@@ -40,6 +40,8 @@ const boost::container::flat_map<const char*, ExportTemplate, CmpStr>
     exportTemplates{
         {{"EEPROM", ExportTemplate("eeprom $Address",
                                    "/sys/bus/i2c/devices/i2c-$Bus/new_device")},
+         {"24C02", ExportTemplate("24c02 $Address",
+                                  "/sys/bus/i2c/devices/i2c-$Bus/new_device")},
          {"24C64", ExportTemplate("24c64 $Address",
                                   "/sys/bus/i2c/devices/i2c-$Bus/new_device")},
          {"Gpio", ExportTemplate("$Index", "/sys/class/gpio/export")},
