0e98142d14dadb74ba13d0feb8fa362e885f85ef
Devjit Gopalpur <devjitg@google.com>
2019-10-10 22:47:46 -0700

    Enable INA230 current sensor and PCA9547 I2C mux
    Signed-off-by: Devjit Gopalpur <devjitg@google.com>
    Change-Id: I994a8fee8660caf3ca60310aa2f8ac3f618e764e

--- include/devices.hpp
+++ include/devices.hpp
@@ -57,6 +57,9 @@ const boost::container::flat_map<const char*, ExportTemplate, CmpStr>
          {"PCA9546Mux",
           ExportTemplate("pca9546 $Address",
                          "/sys/bus/i2c/devices/i2c-$Bus/new_device")},
+         {"PCA9547Mux",
+          ExportTemplate("pca9547 $Address",
+                         "/sys/bus/i2c/devices/i2c-$Bus/new_device")},
          {"MAX31725",
           ExportTemplate("max31725 $Address",
                          "/sys/bus/i2c/devices/i2c-$Bus/new_device")},
@@ -75,5 +78,8 @@ const boost::container::flat_map<const char*, ExportTemplate, CmpStr>
                          "/sys/bus/i2c/devices/i2c-$Bus/new_device")},
          {"TMP112",
           ExportTemplate("tmp112 $Address",
+                         "/sys/bus/i2c/devices/i2c-$Bus/new_device")},
+         {"INA230",
+          ExportTemplate("ina230 $Address",
                          "/sys/bus/i2c/devices/i2c-$Bus/new_device")}}};
 } // namespace devices
