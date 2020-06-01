0e7c447893b2733747c2cfebb03ca8d345de6e55
Patrick Williams <patrick@stwcx.xyz>
2020-06-01 09:30:47 -0500

    sdbusplus: use meson build
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I2ba6002911b35dd67010d981aedaf25c0fc56fc0

--- build-sdbusplus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-if [ ! -d $MESON_BUILD_ROOT/sdbusplus ]; then
-    cp -r $MESON_SOURCE_ROOT/subprojects/sdbusplus $MESON_BUILD_ROOT
-    cd $MESON_BUILD_ROOT/sdbusplus
-    ./bootstrap.sh
-    ./configure --enable-transaction
-    make -j libsdbusplus.la
-fi
