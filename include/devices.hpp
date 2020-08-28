1fb9f3ff893572d9255ea235b7c73edd362efdc5
Brad Bishop <bradleyb@fuzziesquirrel.com>
2020-08-28 08:15:13 -0400

    docs: Add minimal doxygen setup
    Add a Doxyfile and the minimum required tags to the sources to make
    anything useful appear in the documentation.
    
    To generate just run: doxygen from the project root or doxygen
    ./Doxyfile.
    
    Change-Id: Ie60eb1458586dc0a7843dca69e969b78817260d3
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- include/devices.hpp
+++ include/devices.hpp
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 */
+/// \file devices.hpp
 
 #pragma once
 #include <boost/container/flat_map.hpp>
