a079407149747e6b3cee4d26cc844224479aad54
Helen Huang <he.huang@intel.com>
2021-02-19 16:18:22 +0800

    Checking  nullptr of busmap[0]
    If no device in i2cbus2 under /dev, busmap[0] would be nullptr.
    So initialize busmap[0] for this case.
    
    Change-Id: I019bd850474af6b1763b8b71c14d73a233fe881a
    Signed-off-by: Helen Huang <he.huang@linux.intel.com>

--- src/FruDevice.cpp
+++ src/FruDevice.cpp
@@ -1489,6 +1489,13 @@ void rescanBusses(
                 std::vector<uint8_t> baseboardFRU;
                 if (readBaseboardFRU(baseboardFRU))
                 {
+                    // If no device on i2c bus 0, the insertion will happen.
+                    auto bus0 =
+                        busmap.try_emplace(0, std::make_shared<DeviceMap>());
+                    if (bus0.second)
+                    {
+                        bus0.first->second->emplace(0, baseboardFRU);
+                    }
                     busmap[0]->emplace(0, baseboardFRU);
                 }
                 for (auto& devicemap : busmap)
