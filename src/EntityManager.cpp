0546339aeeca7fd6277b5daade0c2fed8b752f77
Matt Spinler <spinler@us.ibm.com>
2021-03-23 15:01:10 -0500

    entity-manager: Move main() into separate file
    Doing this allows a testcase to include EntityManager.cpp so that its
    contents can be tested.  If this wasn't done, then the testcase would
    use this main() as the test's main which isn't what is wanted.
    
    Tested:
    No noticed difference in normal operation.
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>
    Change-Id: Id375c387575021f6ad5857fd721e97a45366abc4

--- src/EntityManager.cpp
+++ src/EntityManager.cpp
@@ -42,9 +42,6 @@
 #include <variant>
 constexpr const char* configurationDirectory = PACKAGE_DIR "configurations";
 constexpr const char* schemaDirectory = PACKAGE_DIR "configurations/schemas";
-constexpr const char* tempConfigDir = "/tmp/configuration/";
-constexpr const char* lastConfiguration = "/tmp/configuration/last.json";
-constexpr const char* currentConfiguration = "/var/configuration/system.json";
 constexpr const char* globalSchema = "global.json";
 constexpr const int32_t MAX_MAPPER_DEPTH = 0;
 
@@ -99,7 +96,7 @@ boost::container::flat_map<
 
 // todo: pass this through nicer
 std::shared_ptr<sdbusplus::asio::connection> SYSTEM_BUS;
-static nlohmann::json lastJson;
+nlohmann::json lastJson;
 
 boost::asio::io_context io;
 
@@ -1868,101 +1865,4 @@ void registerCallback(nlohmann::json& systemConfiguration,
         "type='signal',member='PropertiesChanged',path='" + path + "'",
         eventHandler);
     dbusMatches.emplace(path, std::move(match));
-}
-
-int main()
-{
-    // setup connection to dbus
-    SYSTEM_BUS = std::make_shared<sdbusplus::asio::connection>(io);
-    SYSTEM_BUS->request_name("xyz.openbmc_project.EntityManager");
-
-    sdbusplus::asio::object_server objServer(SYSTEM_BUS);
-
-    std::shared_ptr<sdbusplus::asio::dbus_interface> entityIface =
-        objServer.add_interface("/xyz/openbmc_project/EntityManager",
-                                "xyz.openbmc_project.EntityManager");
-
-    // to keep reference to the match / filter objects so they don't get
-    // destroyed
-
-    nlohmann::json systemConfiguration = nlohmann::json::object();
-
-    // We need a poke from DBus for static providers that create all their
-    // objects prior to claiming a well-known name, and thus don't emit any
-    // org.freedesktop.DBus.Properties signals.  Similarly if a process exits
-    // for any reason, expected or otherwise, we'll need a poke to remove
-    // entities from DBus.
-    sdbusplus::bus::match::match nameOwnerChangedMatch(
-        static_cast<sdbusplus::bus::bus&>(*SYSTEM_BUS),
-        sdbusplus::bus::match::rules::nameOwnerChanged(),
-        [&](sdbusplus::message::message&) {
-            propertiesChangedCallback(systemConfiguration, objServer);
-        });
-    // We also need a poke from DBus when new interfaces are created or
-    // destroyed.
-    sdbusplus::bus::match::match interfacesAddedMatch(
-        static_cast<sdbusplus::bus::bus&>(*SYSTEM_BUS),
-        sdbusplus::bus::match::rules::interfacesAdded(),
-        [&](sdbusplus::message::message&) {
-            propertiesChangedCallback(systemConfiguration, objServer);
-        });
-    sdbusplus::bus::match::match interfacesRemovedMatch(
-        static_cast<sdbusplus::bus::bus&>(*SYSTEM_BUS),
-        sdbusplus::bus::match::rules::interfacesRemoved(),
-        [&](sdbusplus::message::message&) {
-            propertiesChangedCallback(systemConfiguration, objServer);
-        });
-
-    io.post(
-        [&]() { propertiesChangedCallback(systemConfiguration, objServer); });
-
-    entityIface->register_method("ReScan", [&]() {
-        propertiesChangedCallback(systemConfiguration, objServer);
-    });
-    entityIface->initialize();
-
-    if (fwVersionIsSame())
-    {
-        if (std::filesystem::is_regular_file(currentConfiguration))
-        {
-            // this file could just be deleted, but it's nice for debug
-            std::filesystem::create_directory(tempConfigDir);
-            std::filesystem::remove(lastConfiguration);
-            std::filesystem::copy(currentConfiguration, lastConfiguration);
-            std::filesystem::remove(currentConfiguration);
-
-            std::ifstream jsonStream(lastConfiguration);
-            if (jsonStream.good())
-            {
-                auto data = nlohmann::json::parse(jsonStream, nullptr, false);
-                if (data.is_discarded())
-                {
-                    std::cerr << "syntax error in " << lastConfiguration
-                              << "\n";
-                }
-                else
-                {
-                    lastJson = std::move(data);
-                }
-            }
-            else
-            {
-                std::cerr << "unable to open " << lastConfiguration << "\n";
-            }
-        }
-    }
-    else
-    {
-        // not an error, just logging at this level to make it in the journal
-        std::cerr << "Clearing previous configuration\n";
-        std::filesystem::remove(currentConfiguration);
-    }
-
-    // some boards only show up after power is on, we want to not say they are
-    // removed until the same state happens
-    setupPowerMatch(SYSTEM_BUS);
-
-    io.run();
-
-    return 0;
 }
