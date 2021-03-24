ae24885c214b9b44698e02201887eca222301a76
Zev Weiss <zweiss@equinix.com>
2021-03-24 22:36:19 +0000

    Add Nuvoton w83773g temp-sensor to devices list
    Tested on ASRock Rack e3c246d4i (an in-progress port) in tandem with
    corresponding dbus-sensors change.
    
    Signed-off-by: Zev Weiss <zweiss@equinix.com>
    Change-Id: Ifd9401b1f0d724890c6e64a8f940fbcc05d52c84

--- include/devices.hpp
+++ include/devices.hpp
@@ -133,5 +133,8 @@ const boost::container::flat_map<const char*, ExportTemplate, CmpStr>
                                    "/sys/bus/i2c/devices/i2c-$Bus/new_device")},
          {"TMP75",
           ExportTemplate("tmp75 $Address",
+                         "/sys/bus/i2c/devices/i2c-$Bus/new_device")},
+         {"W83773G",
+          ExportTemplate("w83773g $Address",
                          "/sys/bus/i2c/devices/i2c-$Bus/new_device")}}};
 } // namespace devices
