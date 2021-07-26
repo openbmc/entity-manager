5d11daf4011efd41cf675968df3ae6cea302b8ef
Scron Chang <Scron.Chang@quantatw.com>
2021-07-26 13:38:26 +0800

    entity-manager: Add max31790 device.
    Add support for max31790.
    
    Tested:
    Entity-manager can instantiate max31790. The hwmon directory and its
    sysfs entries, e.g., fan_input/target and pwm, are created successfully.
    
    Signed-off-by: Scron Chang <Scron.Chang@quantatw.com>
    Change-Id: Ia8b548b1242fcd879c0bfd3c4c446cd7c0da1924

--- include/devices.hpp
+++ include/devices.hpp
@@ -166,5 +166,8 @@ const boost::container::flat_map<const char*, ExportTemplate, CmpStr>
                          "new_device", "delete_device", true)},
          {"DPS800",
           ExportTemplate("dps800 $Address", "/sys/bus/i2c/devices/i2c-$Bus",
+                         "new_device", "delete_device", true)},
+         {"MAX31790",
+          ExportTemplate("max31790 $Address", "/sys/bus/i2c/devices/i2c-$Bus",
                          "new_device", "delete_device", true)}}};
 } // namespace devices
