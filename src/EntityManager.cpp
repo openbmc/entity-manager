855c0223ca07df67c9c9c39779325bf5f15c45d7
Jie Yang <jjy@google.com>
2021-08-16 14:29:44 -0700

    Publish the root entity object path
    When scanning the system I2C topology, entity-manager can perceive the
    motherboard from multiple entity I2C EEPROM. The system I2C topology
    would be determined at run time. Any add-in boards/cards plugged into
    the system via connectors with I2C bus pass-through would be behind a
    motherboard I2C bus. This change makes the entity-manager "ReScan"
    method return the motherboard DBus inventory object path. The
    motherboard object then would be associated with other system resources.
    
    Tested:
    Tested on a system with motherboard + IO risers + PCIE boards. DBus call
    on entity-manager ReScan method returns the motherboard object path.
    
    busctl call xyz.openbmc_project.EntityManager \
    /xyz/openbmc_project/EntityManager xyz.openbmc_project.EntityManager \
    ReScan
    s "/xyz/openbmc_project/inventory/system/board/GSZ"
    
    Signed-off-by: Jie Yang <jjy@google.com>
    Change-Id: Ib71bb00635751cb55e86a406147a60f2493e844b

--- src/EntityManager.cpp
+++ src/EntityManager.cpp
@@ -50,6 +50,8 @@ constexpr const char* tempConfigDir = "/tmp/configuration/";
 constexpr const char* lastConfiguration = "/tmp/configuration/last.json";
 constexpr const char* currentConfiguration = "/var/configuration/system.json";
 constexpr const char* globalSchema = "global.json";
+static std::string rootEntityObjPath;
+
 constexpr const bool debug = false;
 
 const boost::container::flat_map<const char*, probe_type_codes, CmpStr>
@@ -938,6 +940,13 @@ void postToDbus(const nlohmann::json& newConfiguration,
             root.push(i);
         }
     }
+
+    // The motherboard would be the root of I2C topology
+    if (root.size() == 1)
+    {
+        rootEntityObjPath = std::get<0>(indexNodeMap[root.front()]);
+    }
+
     while (!root.empty())
     {
         auto nd = root.front();
@@ -1325,8 +1334,9 @@ int main()
     io.post(
         [&]() { propertiesChangedCallback(systemConfiguration, objServer); });
 
-    entityIface->register_method("ReScan", [&]() {
+    entityIface->register_method("ReScan", [&]() -> std::string {
         propertiesChangedCallback(systemConfiguration, objServer);
+        return rootEntityObjPath;
     });
     entityIface->initialize();
 
