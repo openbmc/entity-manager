19381158674539057d5bae05b2841bad93bc5bfc
Michael Shen <gpgpgp@google.com>
2022-02-22 14:57:38 +0800

    FruDevice: Add SPD format support
    This commit adds the virtual interface for SPD format.
    The reason for those virtual functions is to support different
    generations of DIMM SPD. Because the SPD format has some differences
    between generations. For example `spdUsedSize` and `spdTotalSize` are
    the same in DDR5 while it’s two different fields in DDR4.
    
    Tested:
    Ran unittests for every static function in SPD interface.
    
    Signed-off-by: Michael Shen <gpgpgp@google.com>
    Change-Id: I038854165bf29bf5e4d2392ffe394edbf0b85e61

---
+++ include/spd/spd.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <span>
+#include <string>
+#include <utility>
+#include <vector>
+class Spd
+{
+  public:
+    enum class SpdType
+    {
+        kTypeUnknown = 0x00,
+        kTypeDdr1 = 0x07,
+        kTypeDdr2 = 0x08,
+        kTypeFbdimm = 0x09,
+        kTypeDdr3 = 0x0B,
+        kTypeDdr4 = 0x0C,
+        kTypeDdr5 = 0x12,
+    };
+
+    enum class ModuleType
+    {
+        kTypeUnkonwn = 0x00,
+        kTypeRDIMM = 0x01,
+        kTypeUDIMM = 0x02,
+        kTypeSODIMM = 0x03,
+        kTypeLRDIMM = 0x04,
+        kTypeDDIMM = 0x0A,
+        kTypeSolderDown = 0x0B,
+        kTypeNVNRDIMM = 0x91,
+        kTypeNVPRDIMM = 0x92,
+    };
+
+    struct Date
+    {
+        int32_t year;
+        int32_t week;
+    };
+
+    // Minimal required SPD size.
+    // byte[0] == SPD size, byte[2] == SPD type(DDR1/2/3/4/5 etc..)
+    static constexpr uint32_t kMinRequiredSpdSize = 3;
+
+    // Returns the type of the SPD, or Spd::kTypeUnknown on error.
+    static SpdType getSpdType(std::span<const uint8_t> spdImage);
+
+    // Returns the total size of SPD, or -1 on error.
+    static int32_t getSpdSize(std::span<const uint8_t> spdImage);
+
+    // Parses SPD information from the given source and returns a new Spd object
+    // owned by the caller. Returns NULL on error or if the type of the given
+    // SPD is unsupported.
+    static std::unique_ptr<Spd> getFromImage(std::span<const uint8_t> spdImage);
+
+    // Calculate the crc16 for the SPD content. The crc16 algorithm is defined
+    // in JEDEC
+    static uint16_t calcJedecCrc16(std::span<const uint8_t> spdImage);
+
+    virtual ~Spd() = default;
+
+    // Returns the type of this Spd, or kTypeUnknown if the Spd has not been
+    // initialized with FillFromImage or the type is unknown.
+    virtual SpdType type() const = 0;
+
+    // Returns a copy of the raw bytes of this Spd.
+    virtual std::span<const uint8_t> raw() const = 0;
+
+    // Returns the number of bytes used by the module manufacturer in this Spd.
+    virtual int32_t spdUsedSize() const = 0;
+
+    // Returns the total size of the serial memory used to hold the Spd data.
+    virtual int32_t spdTotalSize() const = 0;
+
+    // Manufacturer ID output can be interpreted according to JEDEC's
+    // manufacturer ID list (JEP106).
+    // pair.first == bank number, pair.second == offset in the bank.
+    virtual std::pair<int8_t, int8_t> manufacturerId() const = 0;
+    virtual int8_t manufacturerLocation() const = 0;
+
+    // pair.first == the full year number (e.g. 2006)
+    // pair.second == the number of the week in the year (e.g. 14).
+    virtual Date manufacturingDate() const = 0;
+    virtual ModuleType module() const = 0;
+    virtual std::vector<uint8_t> serialNumber() const = 0;
+    virtual std::string partNumber() const = 0;
+    virtual int8_t revisionCode() const = 0;
+    virtual int64_t dimmSize() const = 0; // in MiB
+    virtual int8_t eccBits() const = 0;
+    virtual int32_t banks() const = 0;
+    virtual int32_t rows() const = 0;
+    virtual int32_t columns() const = 0;
+    // Returns the total number of logical ranks of the DIMM.
+    virtual int32_t ranks() const = 0;
+    virtual int32_t width() const = 0;
+    virtual uint16_t checksum() const = 0; // Stored (not computed) checksum.
+};
---
+++ src/spd.cpp
@@ -0,0 +1,60 @@
+#include "spd/spd.hpp"
+
+#include <boost/crc.hpp>
+
+#include <iostream>
+#include <span>
+
+Spd::SpdType Spd::getSpdType(std::span<const uint8_t> spdImage)
+{
+    // Spd type info is located in byte 2.
+    if (spdImage.size() < kMinRequiredSpdSize)
+    {
+        return SpdType::kTypeUnknown;
+    }
+    return static_cast<SpdType>(spdImage[2]);
+}
+
+int32_t Spd::getSpdSize(std::span<const uint8_t> spdImage)
+{
+    // SPD size info is located in byte 0.
+    if (spdImage.size() < kMinRequiredSpdSize)
+    {
+        return -1;
+    }
+
+    const uint8_t kByteTotal = (spdImage[0] >> 4) & 0x07;
+    if (kByteTotal > 0x03 || kByteTotal < 0x01)
+    {
+        return -1;
+    }
+    return (1 << kByteTotal) * 128;
+}
+
+std::unique_ptr<Spd> Spd::getFromImage(std::span<const uint8_t> spdImage)
+{
+    switch (getSpdType(spdImage))
+    {
+        case SpdType::kTypeDdr1:
+        case SpdType::kTypeDdr2:
+        case SpdType::kTypeFbdimm:
+        case SpdType::kTypeDdr3:
+        case SpdType::kTypeDdr4:
+        case SpdType::kTypeDdr5:
+            std::cerr << "Unsupported SPD type: 0x" << std::hex
+                      << static_cast<uint8_t>(getSpdType(spdImage)) << "\n";
+            return nullptr;
+        default:
+            std::cerr << "Unrecognized SPD type: 0x" << std::hex
+                      << static_cast<uint8_t>(getSpdType(spdImage)) << "\n";
+            return nullptr;
+    }
+}
+
+uint16_t Spd::calcJedecCrc16(std::span<const uint8_t> data)
+{
+    // crc_xmodem_t == crc_optimal<16, 0x1021, 0, 0, false, false>
+    boost::crc_xmodem_t result;
+    result.process_bytes(data.data(), data.size());
+    return result.checksum();
+}
