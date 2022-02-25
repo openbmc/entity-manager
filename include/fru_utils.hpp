db2c33c729b59a059b1867bba74764043348a465
Kumar Thangavel <thangavel.k@hcl.com>
2022-02-25 16:20:10 +0530

    Move common functions to FruUtils.cpp file.
    Move the common functions addFruObjectToDbus, updateFRUProperty
    and isMuxBus from FruDevice.cpp to FruUtils.cpp for handling fru
    daemon and ipmb-fru deamon avoid code duplication in entity-manager.
    
    To avoid CI Issue in test_fru_utils, moved main function to
    FRUDeviceMain.cpp file.
    
    TESTED : Built Facebook YosemiteV2 images and loaded
    on the target hardware. Verified all the fru's read and write.
    
    Signed-off-by: Kumar Thangavel <thangavel.k@hcl.com>
    Change-Id: Ic0cbebd00f896117d59c86b97fe6b6f217b7ba95

--- include/fru_utils.hpp
+++ include/fru_utils.hpp
@@ -18,6 +18,7 @@
 #pragma once
 #include "fru_reader.hpp"
 #include <boost/container/flat_map.hpp>
+#include <sdbusplus/asio/object_server.hpp>
 
 #include <cstdint>
 #include <functional>
@@ -156,3 +157,34 @@ bool validateHeader(const std::array<uint8_t, I2C_SMBUS_BLOCK_MAX>& blockData);
 /// \param area - the area
 /// \return the field offset
 unsigned int getHeaderAreaFieldOffset(fruAreas area);
+
+// Details with example of Asset Tag Update
+// To find location of Product Info Area asset tag as per FRU specification
+// 1. Find product Info area starting offset (*8 - as header will be in
+// multiple of 8 bytes).
+// 2. Skip 3 bytes of product info area (like format version, area length,
+// and language code).
+// 3. Traverse manufacturer name, product name, product version, & product
+// serial number, by reading type/length code to reach the Asset Tag.
+// 4. Update the Asset Tag, reposition the product Info area in multiple of
+// 8 bytes. Update the Product area length and checksum.
+
+bool updateFRUProperty(
+    const std::string& updatePropertyReq, uint32_t bus, uint32_t address,
+    const std::string& propertyName,
+    boost::container::flat_map<
+        std::pair<size_t, size_t>,
+        std::shared_ptr<sdbusplus::asio::dbus_interface>>& dbusInterfaceMap,
+    size_t& unknownBusObjectCount, const bool& powerIsOn,
+    sdbusplus::asio::object_server& objServer,
+    std::shared_ptr<sdbusplus::asio::connection>& systemBus, bool ipmbFlag);
+
+void addFruObjectToDbus(
+    std::vector<uint8_t>& device,
+    boost::container::flat_map<
+        std::pair<size_t, size_t>,
+        std::shared_ptr<sdbusplus::asio::dbus_interface>>& dbusInterfaceMap,
+    uint32_t bus, uint32_t address, size_t& unknownBusObjectCount,
+    const bool& powerIsOn, sdbusplus::asio::object_server& objServer,
+    std::shared_ptr<sdbusplus::asio::connection>& systemBus, int ipmbIndex = -1,
+    bool ipmbFlag = false);
