44e340945eef08fae2cb42ed9b046d0fd155c831
Benjamin Fair <benjaminfair@google.com>
2022-04-26 14:25:12 -0700

    Topology: WIP
    Signed-off-by: Benjamin Fair <benjaminfair@google.com>
    Change-Id: I16ebf4942219eb2f277635f6175fac913fa45725

---
+++ include/Topology.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+class Topology
+{
+  public:
+    using Association = std::tuple<std::string, std::string, std::string>;
+
+    Topology(const sdbusplus::message::object_path& root) : root(root)
+    {}
+
+    std::vector<Association> dbusProp()
+    {
+        std::vector<Association> prop;
+
+        if (root == "/xyz/openbmc_project/inventory/system/chassis/Subchassis") {
+            prop.emplace_back("chassisContainedBy", "chassisContains",
+                              "/xyz/openbmc_project/inventory/system/chassis/Superchassis");
+        }
+
+        return prop;
+    }
+
+  private:
+    sdbusplus::message::object_path root;
+};
