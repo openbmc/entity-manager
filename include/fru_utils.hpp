f57fe610d9886e548b074f0b43595ddf6559e147
Kumar Thangavel <thangavel.k@hcl.com>
2022-08-10 18:05:20 +0530

    Refactor : Add a helper function to addFruObjectToDbus
    Refactoring addFruObjectToDbus function and created a new helper
    function formatFRUFind. Moved this function to fru_utils.cpp as
    it is common for all fru-device deamons and avoid code duplication.
    
    This patch is created based on suggestion on the below patch.
    https://gerrit.openbmc.org/c/openbmc/entity-manager/+/51555
    
    TESTED : Built Facebook YosemiteV2 images and loaded
    on the target hardware. Verified all the fru's read and write.
    
    Signed-off-by: Kumar Thangavel <thangavel.k@hcl.com>
    Change-Id: I272d2fd2fc664a471774c668503d87147eb80a54

--- include/fru_utils.hpp
+++ include/fru_utils.hpp
@@ -157,3 +157,8 @@ bool validateHeader(const std::array<uint8_t, I2C_SMBUS_BLOCK_MAX>& blockData);
 /// \param area - the area
 /// \return the field offset
 unsigned int getHeaderAreaFieldOffset(fruAreas area);
+
+std::optional<std::string> formatFRUFind(
+    std::vector<uint8_t>& device,
+    boost::container::flat_map<std::string, std::string>& formattedFRU,
+    uint32_t bus, uint32_t address, size_t& unknownBusObjectCount);
