d504a9cd523eab7a417339760e79385c0b5b7132
Sui Chen <suichen@google.com>
2022-08-24 22:39:35 -0700

    entity-manager: Allow exposing a BMC inventory item
    Recently there appeared use cases involving the usage of a BMC Inventory
    item. One example is the utilization sensors in phosphor-health-monitor;
    those sensors need to be connected to the BMC inventory item via an
    association edge so they can be found by bmcweb.
    
    Based on the conventions of OpenBMC, when a daemon owns a BMC Inventory
    item, it is expected to have a complete understanding of the underlying
    object. As such, Entity-Manager is in a better position to own the BMC
    Inventory item compared to any other daemon.
    
    Customarily, an Inventory item must always refer to a tangible, physical
    object such as a board. Because of this, the existence of a BMC
    Inventory item is an exception. Because in most cases a BMC is not a
    discrete physical object and is usually embedded into another physical
    component, such as a DC-SCM module or a Board, the information regarding
    a BMC must be encoded in the board's JSON file.
    
    As such, this change adds a "HasThisBMC" key that can be added to a JSON
    file to indicate that the particular JSON file contains the BMC that
    runs the current OpenBMC distribution.
    
    Tested: After adding 'HasThisBMC: true' to a board .JSON file, the BMC
    item shows up:
    
    bmc# busctl tree xyz.openbmc_project.EntityManager
    `-/xyz
      `-/xyz/openbmc_project
        |-/xyz/openbmc_project/EntityManager
        `-/xyz/openbmc_project/inventory
          `-/xyz/openbmc_project/inventory/system
            |-/xyz/openbmc_project/inventory/system/board
            | `-/xyz/openbmc_project/inventory/system/board/SuperBoard
            |   `-/xyz/openbmc_project/inventory/system/board/SuperBoard/BMC
    
    Signed-off-by: Sui Chen <suichen@google.com>
    Change-Id: Iaf3595d4c336dbcf1f4e42f4aaa2c6c1840f9584

--- src/entity_manager.cpp
+++ src/entity_manager.cpp
@@ -628,6 +628,26 @@ void postToDbus(const nlohmann::json& newConfiguration,
             }
         }
 
+        // "ContainsThisBMC"
+        auto findHasThisBMC = boardValues.find("ContainsThisBMC");
+        if (findHasThisBMC != boardValues.end())
+        {
+            // Create a "BMC" sub-item with an
+            // xyz.openbmc_project.Inventory.Item.Bmc interface
+            std::shared_ptr<sdbusplus::asio::dbus_interface> bmcIface =
+                createInterface(objServer, boardName + "/BMC",
+                                "xyz.openbmc_project.Inventory.Item.Bmc",
+                                boardName);
+
+            // Populate an "xyz.openbmc_project.Bmc" interface for this sub-item
+            // Do not write system JSON since this interface is generated
+            // on-the-fly
+            bmcIface->register_property(
+                "Type", std::string("xyz.openbmc_project.Inventory.Item.Bmc"));
+            bmcIface->register_property("Name", std::string("BMC"));
+            bmcIface->initialize();
+        }
+
         auto exposes = boardValues.find("Exposes");
         if (exposes == boardValues.end())
         {
