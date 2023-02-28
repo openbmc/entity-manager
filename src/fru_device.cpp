aa497edfc5dd4bcc7e3508498378d0747c622088
Ed Tanous <edtanous@google.com>
2023-02-28 13:43:41 -0800

    Use steady_timer instead of deadline timer
    In boost, deadline_timer runs off the REALTIME clock, whereas
    steady_timer runs off the MONOTONIC clock.  Because this timer should be
    based on a future time duration, not an exact calendar date, we should
    be using steady_timer here.
    
    The difference between the two is subtle, and largely doesn't matter
    unless the date/time is set, in which case deadline_timer won't give the
    expected result.
    
    Change-Id: I72e85a81b3a8e53161135befc6f42079ba36be6f
    Signed-off-by: Ed Tanous <edtanous@google.com>

--- src/fru_device.cpp
+++ src/fru_device.cpp
@@ -23,8 +23,8 @@
 #include <sys/ioctl.h>
 
 #include <boost/algorithm/string/predicate.hpp>
-#include <boost/asio/deadline_timer.hpp>
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/steady_timer.hpp>
 #include <boost/container/flat_map.hpp>
 #include <nlohmann/json.hpp>
 #include <sdbusplus/asio/connection.hpp>
@@ -939,8 +939,8 @@ void rescanBusses(
     sdbusplus::asio::object_server& objServer,
     std::shared_ptr<sdbusplus::asio::connection>& systemBus)
 {
-    static boost::asio::deadline_timer timer(io);
-    timer.expires_from_now(boost::posix_time::seconds(1));
+    static boost::asio::steady_timer timer(io);
+    timer.expires_from_now(std::chrono::seconds(1));
 
     // setup an async wait in case we get flooded with requests
     timer.async_wait([&](const boost::system::error_code& ec) {
