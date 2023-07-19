68525707e90ee2e670d81f3e02b8a07f247eb889
Adi Fogel <afogel@nvidia.com>
2023-07-19 11:44:58 +0000

    Remove spaces from the end of fru field name
    The EEPROM string may contain spaces in the field name.
    When those string are used in redfish, for example:
     "SerialNumber": "MT2333X02531            ",
    the json output result will also contain those unnecessary spaces.
    The spaces in redfish cannot be ignored by client and it is easier
    to remove them at the dbus-object level, in fru device.
    Test:
    "SerialNumber": "MT2345300006"
    
    Change-Id: Ia420bd5878bf34639a0b576e9de6edab48babeac
    Signed-off-by: Adi Fogel <afogel@nvidia.com>

--- src/fru_device.cpp
+++ src/fru_device.cpp
@@ -795,12 +795,15 @@ void addFruObjectToDbus(
         }
         std::string key = std::regex_replace(property.first, nonAsciiRegex,
                                              "_");
-
+        std::string value = property.second;
+        // Remove the spaces from the end of the key string
+        value.erase(std::find_if(value.rbegin(), value.rend(), 
+            [](unsigned char ch) {return !std::isspace(ch);}).base(), value.end());
         if (property.first == "PRODUCT_ASSET_TAG")
         {
             std::string propertyName = property.first;
             iface->register_property(
-                key, property.second + '\0',
+                key, value + '\0',
                 [bus, address, propertyName, &dbusInterfaceMap,
                  &unknownBusObjectCount, &powerIsOn, &objServer,
                  &systemBus](const std::string& req, std::string& resp) {
@@ -829,7 +832,7 @@ void addFruObjectToDbus(
         }
         if (debug)
         {
-            std::cout << property.first << ": " << property.second << "\n";
+            std::cout << property.first << ": " << value << "\n";
         }
     }
 
