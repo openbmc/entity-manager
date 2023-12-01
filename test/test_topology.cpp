1d112e8cab2d7f0f87597943c742a4dfeefbe381
Jeff Lin <JeffLin2@quantatw.com>
2023-12-01 11:23:54 +0800

    topology: Add powered_by topology
    bmcweb already support the power supply command, refer to
    https://github.com/openbmc/bmcweb/blob/master/redfish-core/lib/power_supply.hpp#L73
    
    Add PoweredPort port to add the powered_by assocition to the target
    power supply board.
    
    Tested:
    1. Run unit test of test_topology and result is Passed.
    2. Test on real machine
    Add the following config in mobo's json:
    ```
    {
        "Name": "PSU Upstream Port",
        "Type": "PSU Upstream Port"
    },
    {
        "PoweredByType": "PSU Power Port",
        "Name": "PSU Port",
        "Type": "PoweredPort"
    },
    ```
    Add the following config in power supply's json:
    ```
    {
        "ConnectsToType": "PSU Upstream Port",
        "Name": "PSU Port",
        "Type": "DownstreamPort"
    },
    {
        "Name": "PSU Power Port",
        "Type": "PSU Power Port"
    },
    ```
    Result:
    root@qbmc:~# busctl get-property xyz.openbmc_project.EntityManager /xyz/openbmc_project/inventory/system/board/mobo xyz.openbmc_project.Association.Definitions Associations -j
    {
    	"type" : "a(sss)",
    	"data" : [
    		[
    			"powered_by",
    			"powering",
    			"/xyz/openbmc_project/inventory/system/board/PSU"
    		]
    	]
    }
    root@qbmc:~# busctl get-property xyz.openbmc_project.EntityManager /xyz/openbmc_project/inventory/system/board/PSU xyz.openbmc_project.Association.Definitions Associations -j
    {
    	"type" : "a(sss)",
    	"data" : [
    		[
    			"contained_by",
    			"containing",
    			"/xyz/openbmc_project/inventory/system/board/mobo"
    		]
    	]
    }
    $ curl -k -H "X-Auth-Token: $token" -X GET http://${bmc}/redfish/v1/Chassis/mobo/PowerSubsystem/PowerSupplies/PSU
    {
      "@odata.id": "/redfish/v1/Chassis/mobo/PowerSubsystem/PowerSupplies/PSU",
      "@odata.type": "#PowerSupply.v1_5_0.PowerSupply",
      "EfficiencyRatings": [
        {
          "EfficiencyPercent": 90
        }
      ],
      "Id": "PSU",
      "Manufacturer": "Quanta",
      "Model": "PSU",
      "Name": "Power Supply",
      "PartNumber": "1092350-07",
      "SerialNumber": "IPBQTW222000005",
      "Status": {
        "Health": "OK",
        "State": "Enabled"
      }
    }
    ```
    
    Change-Id: Iad10e61417437a41628cf311cdd7893725a5dcde
    Signed-off-by: Jeff Lin <JeffLin2@quantatw.com>

--- test/test_topology.cpp
+++ test/test_topology.cpp
@@ -9,9 +9,13 @@ const std::string subchassisPath =
     "/xyz/openbmc_project/inventory/system/chassis/Subchassis";
 const std::string superchassisPath =
     "/xyz/openbmc_project/inventory/system/chassis/Superchassis";
+const std::string superpowerPath =
+    "/xyz/openbmc_project/inventory/system/chassis/Superpower";
 
 const Association subchassisAssoc =
     std::make_tuple("contained_by", "containing", superchassisPath);
+const Association subPowerAssoc = std::make_tuple("powered_by", "powering",
+                                                  superpowerPath);
 
 const nlohmann::json subchassisExposesItem = nlohmann::json::parse(R"(
     {
@@ -21,10 +25,25 @@ const nlohmann::json subchassisExposesItem = nlohmann::json::parse(R"(
     }
 )");
 
+const nlohmann::json subpowerExposesItem = nlohmann::json::parse(R"(
+    {
+        "PoweredByType": "PowerSupplyPort",
+        "Name": "MyPoweredPort",
+        "Type": "PoweredPort"
+    }
+)");
+
 const nlohmann::json superchassisExposesItem = nlohmann::json::parse(R"(
     {
         "Name": "MyBackplanePort",
         "Type": "BackplanePort"
+    }
+)");
+
+const nlohmann::json superpowerExposesItem = nlohmann::json::parse(R"(
+    {
+        "Name": "PowerSupplyPort",
+        "Type": "PowerSupplyPort"
     }
 )");
 
@@ -82,6 +101,28 @@ TEST(Topology, MissingConnectsTo)
     EXPECT_EQ(assocs.size(), 0);
 }
 
+TEST(Topology, MissingPoweredByType)
+{
+    const nlohmann::json subchassisMissingPoweredByType =
+        nlohmann::json::parse(R"(
+        {
+            "Name": "MyDownstreamPort",
+            "Type": "DownstreamPort"
+        }
+    )");
+
+    Topology topo;
+    BoardMap boards{{subchassisPath, "BoardA"}, {superchassisPath, "BoardB"}};
+
+    topo.addBoard(subchassisPath, "Chassis", "BoardA",
+                  subchassisMissingPoweredByType);
+    topo.addBoard(superchassisPath, "Chassis", "BoardB", subpowerExposesItem);
+
+    auto assocs = topo.getAssocs(boards);
+
+    EXPECT_EQ(assocs.size(), 0);
+}
+
 TEST(Topology, OtherExposes)
 {
     Topology topo;
@@ -98,11 +139,15 @@ TEST(Topology, OtherExposes)
 TEST(Topology, NoMatchSubchassis)
 {
     Topology topo;
-    BoardMap boards{{subchassisPath, "BoardA"}, {superchassisPath, "BoardB"}};
+
+    BoardMap boards{{subchassisPath, "BoardA"},
+                    {superchassisPath, "BoardB"},
+                    {superpowerPath, "BoardC"}};
 
     topo.addBoard(subchassisPath, "Chassis", "BoardA", otherExposesItem);
     topo.addBoard(superchassisPath, "Chassis", "BoardB",
                   superchassisExposesItem);
+    topo.addBoard(superpowerPath, "Chassis", "BoardC", superpowerExposesItem);
 
     auto assocs = topo.getAssocs(boards);
 
@@ -112,17 +157,22 @@ TEST(Topology, NoMatchSubchassis)
 TEST(Topology, NoMatchSuperchassis)
 {
     Topology topo;
-    BoardMap boards{{subchassisPath, "BoardA"}, {superchassisPath, "BoardB"}};
+    BoardMap boards{{subchassisPath, "BoardA"},
+                    {subchassisPath, "BoardB"},
+                    {superchassisPath, "BoardC"},
+                    {superpowerPath, "BoardD"}};
 
     topo.addBoard(subchassisPath, "Chassis", "BoardA", subchassisExposesItem);
-    topo.addBoard(superchassisPath, "Chassis", "BoardB", otherExposesItem);
+    topo.addBoard(subchassisPath, "Chassis", "BoardB", subpowerExposesItem);
+    topo.addBoard(superchassisPath, "Chassis", "BoardC", otherExposesItem);
+    topo.addBoard(superpowerPath, "Chassis", "BoardD", otherExposesItem);
 
     auto assocs = topo.getAssocs(boards);
 
     EXPECT_EQ(assocs.size(), 0);
 }
 
-TEST(Topology, Basic)
+TEST(Topology, BasicChassis)
 {
     Topology topo;
     BoardMap boards{{subchassisPath, "BoardA"}, {superchassisPath, "BoardB"}};
@@ -138,14 +188,31 @@ TEST(Topology, Basic)
     EXPECT_EQ(assocs[subchassisPath][0], subchassisAssoc);
 }
 
+TEST(Topology, BasicPower)
+{
+    Topology topo;
+    BoardMap boards{{subchassisPath, "BoardA"}, {superpowerPath, "BoardB"}};
+
+    topo.addBoard(subchassisPath, "Chassis", "BoardA", subpowerExposesItem);
+    topo.addBoard(superpowerPath, "Chassis", "BoardB", superpowerExposesItem);
+
+    auto assocs = topo.getAssocs(boards);
+
+    EXPECT_EQ(assocs.size(), 1);
+    EXPECT_EQ(assocs[subchassisPath].size(), 1);
+    EXPECT_EQ(assocs[subchassisPath][0], subPowerAssoc);
+}
+
 TEST(Topology, NoNewBoards)
 {
     Topology topo;
     BoardMap boards;
 
     topo.addBoard(subchassisPath, "Chassis", "BoardA", subchassisExposesItem);
-    topo.addBoard(superchassisPath, "Chassis", "BoardB",
+    topo.addBoard(subchassisPath, "Chassis", "BoardB", subpowerExposesItem);
+    topo.addBoard(superchassisPath, "Chassis", "BoardC",
                   superchassisExposesItem);
+    topo.addBoard(superpowerPath, "Chassis", "BoardD", superpowerExposesItem);
 
     // Boards A and B aren't new, so no assocs are returned.
     auto assocs = topo.getAssocs(boards);
@@ -175,7 +242,29 @@ TEST(Topology, 2Subchassis)
     EXPECT_EQ(assocs[subchassisPath + "2"][0], subchassisAssoc);
 }
 
-TEST(Topology, OneNewBoard)
+TEST(Topology, 2Subpowerchassis)
+{
+    Topology topo;
+    BoardMap boards{{subchassisPath, "BoardA"},
+                    {subchassisPath + "2", "BoardB"},
+                    {superpowerPath, "BoardC"}};
+
+    topo.addBoard(subchassisPath, "Chassis", "BoardA", subpowerExposesItem);
+    topo.addBoard(subchassisPath + "2", "Chassis", "BoardB",
+                  subpowerExposesItem);
+    topo.addBoard(superpowerPath, "Chassis", "BoardC",
+                  superpowerExposesItem);
+
+    auto assocs = topo.getAssocs(boards);
+
+    EXPECT_EQ(assocs.size(), 2);
+    EXPECT_EQ(assocs[subchassisPath].size(), 1);
+    EXPECT_EQ(assocs[subchassisPath][0], subPowerAssoc);
+    EXPECT_EQ(assocs[subchassisPath + "2"].size(), 1);
+    EXPECT_EQ(assocs[subchassisPath + "2"][0], subPowerAssoc);
+}
+
+TEST(Topology, OneNewChassisBoard)
 {
     Topology topo;
     BoardMap boards{{subchassisPath, "BoardA"}};
@@ -192,6 +281,72 @@ TEST(Topology, OneNewBoard)
     EXPECT_EQ(assocs.size(), 1);
     EXPECT_EQ(assocs[subchassisPath].size(), 1);
     EXPECT_EQ(assocs[subchassisPath][0], subchassisAssoc);
+}
+
+TEST(Topology, OneNewPoweredBoard)
+{
+    Topology topo;
+    BoardMap boards{{subchassisPath, "BoardA"}};
+
+    topo.addBoard(subchassisPath, "Chassis", "BoardA", subpowerExposesItem);
+    topo.addBoard(subchassisPath + "2", "Chassis", "BoardB",
+                  subpowerExposesItem);
+    topo.addBoard(superpowerPath, "Chassis", "BoardC", superpowerExposesItem);
+
+    // Only the assoc for BoardB should be returned
+    auto assocs = topo.getAssocs(boards);
+
+    EXPECT_EQ(assocs.size(), 1);
+    EXPECT_EQ(assocs[subchassisPath].size(), 1);
+    EXPECT_EQ(assocs[subchassisPath][0], subPowerAssoc);
+}
+
+TEST(Topology, OneNewPoweredBoardAndOneNewChassisBoard)
+{
+    Topology topo;
+    BoardMap boards{{subchassisPath, "BoardA"},
+                    {subchassisPath + "3", "BoardC"}};
+
+    topo.addBoard(subchassisPath, "Chassis", "BoardA", subchassisExposesItem);
+    topo.addBoard(subchassisPath + "2", "Chassis", "BoardB",
+                  subchassisExposesItem);
+    topo.addBoard(subchassisPath + "3", "Chassis", "BoardC",
+                  subpowerExposesItem);
+    topo.addBoard(subchassisPath + "4", "Chassis", "BoardD",
+                  subpowerExposesItem);
+    topo.addBoard(superchassisPath, "Chassis", "BoardE",
+                  superchassisExposesItem);
+    topo.addBoard(superpowerPath, "Chassis", "BoardF", superpowerExposesItem);
+
+    // Only the assoc for BoardA and BoardC should be returned
+    auto assocs = topo.getAssocs(boards);
+
+    EXPECT_EQ(assocs.size(), 2);
+    EXPECT_EQ(assocs[subchassisPath].size(), 1);
+    EXPECT_EQ(assocs[subchassisPath][0], subchassisAssoc);
+    EXPECT_EQ(assocs[subchassisPath + "3"].size(), 1);
+    EXPECT_EQ(assocs[subchassisPath + "3"][0], subPowerAssoc);
+}
+
+TEST(Topology, PoweredAndConteinedByEachOther)
+{
+    Topology topo;
+    BoardMap boards{{superchassisPath, "BoardA"}, {superpowerPath, "BoardB"}};
+
+    // BoardA powered_by BoardB and BoardB contained_by BoardA
+    topo.addBoard(superchassisPath, "Chassis", "BoardA", subpowerExposesItem);
+    topo.addBoard(superchassisPath, "Chassis", "BoardA",
+                  superchassisExposesItem);
+    topo.addBoard(superpowerPath, "Chassis", "BoardB", subchassisExposesItem);
+    topo.addBoard(superpowerPath, "Chassis", "BoardB", superpowerExposesItem);
+
+    auto assocs = topo.getAssocs(boards);
+
+    EXPECT_EQ(assocs.size(), 2);
+    EXPECT_EQ(assocs[superchassisPath].size(), 1);
+    EXPECT_EQ(assocs[superchassisPath][0], subPowerAssoc);
+    EXPECT_EQ(assocs[superpowerPath].size(), 1);
+    EXPECT_EQ(assocs[superpowerPath][0], subchassisAssoc);
 }
 
 TEST(Topology, 2Superchassis)
@@ -217,6 +372,30 @@ TEST(Topology, 2Superchassis)
 
     EXPECT_THAT(assocs[subchassisPath],
                 UnorderedElementsAre(subchassisAssoc, subchassisAssoc2));
+}
+
+TEST(Topology, 2Superpower)
+{
+    const Association subPowerAssoc2 = std::make_tuple("powered_by", "powering",
+                                                       superpowerPath + "2");
+
+    Topology topo;
+    BoardMap boards{{subchassisPath, "BoardA"},
+                    {superpowerPath, "BoardB"},
+                    {superpowerPath + "2", "BoardC"}};
+
+    topo.addBoard(subchassisPath, "Chassis", "BoardA", subpowerExposesItem);
+    topo.addBoard(superpowerPath, "Chassis", "BoardB", superpowerExposesItem);
+    topo.addBoard(superpowerPath + "2", "Chassis", "BoardC",
+                  superpowerExposesItem);
+
+    auto assocs = topo.getAssocs(boards);
+
+    EXPECT_EQ(assocs.size(), 1);
+    EXPECT_EQ(assocs[subchassisPath].size(), 2);
+
+    EXPECT_THAT(assocs[subchassisPath],
+                UnorderedElementsAre(subPowerAssoc, subPowerAssoc2));
 }
 
 TEST(Topology, 2SuperchassisAnd2Subchassis)
@@ -248,6 +427,36 @@ TEST(Topology, 2SuperchassisAnd2Subchassis)
                 UnorderedElementsAre(subchassisAssoc, subchassisAssoc2));
     EXPECT_THAT(assocs[subchassisPath + "2"],
                 UnorderedElementsAre(subchassisAssoc, subchassisAssoc2));
+}
+
+TEST(Topology, 2SuperpowerAnd2Subchassis)
+{
+    const Association subPowerAssoc2 = std::make_tuple("powered_by", "powering",
+                                                       superpowerPath + "2");
+
+    Topology topo;
+    BoardMap boards{{subchassisPath, "BoardA"},
+                    {subchassisPath + "2", "BoardB"},
+                    {superpowerPath, "BoardC"},
+                    {superpowerPath + "2", "BoardD"}};
+
+    topo.addBoard(subchassisPath, "Chassis", "BoardA", subpowerExposesItem);
+    topo.addBoard(subchassisPath + "2", "Chassis", "BoardB",
+                  subpowerExposesItem);
+    topo.addBoard(superpowerPath, "Chassis", "BoardC", superpowerExposesItem);
+    topo.addBoard(superpowerPath + "2", "Chassis", "BoardD",
+                  superpowerExposesItem);
+
+    auto assocs = topo.getAssocs(boards);
+
+    EXPECT_EQ(assocs.size(), 2);
+    EXPECT_EQ(assocs[subchassisPath].size(), 2);
+    EXPECT_EQ(assocs[subchassisPath + "2"].size(), 2);
+
+    EXPECT_THAT(assocs[subchassisPath],
+                UnorderedElementsAre(subPowerAssoc, subPowerAssoc2));
+    EXPECT_THAT(assocs[subchassisPath + "2"],
+                UnorderedElementsAre(subPowerAssoc, subPowerAssoc2));
 }
 
 TEST(Topology, Remove)
