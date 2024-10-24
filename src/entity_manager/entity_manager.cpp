f2803164170e31a4041a11dc6a89fff50744ad6f
Jonico Eustaquio <jonico.eustaquio@fii-na.com>
2024-10-24 14:13:26 -0500

    Support MuxChannel object for dynamic buses
    Since mux devices can be configured in entity-manager, the logical
    buses are dynamically numerated. The MuxChannel object seeks to allow
    devices/sensors behind that mux to be configured in entity-manager with
    its "MuxName" and "ChannelName" properties. The "MuxName" property shall
    match the "Name" property of a mux configured in entity-manager and the
    "ChannelName" property will match a string within the ChannelName array
    of that same mux.
    
    The DeviceConfig class is created in overlay.cpp/hpp to keep keep track
    of the entity-manager configurations and more easily manage devices that
    can be bound. Each DeviceConfig object will be attempted to be bound if
    the device is in the exportTemplates map in devices.hpp.
    
    When a mux is configured in entity-manager with defined ChannelNames, it
    will be bound and a symbolic link is created between
    /dev/<MuxName>/<ChannelName> and /dev/i2c-<bus> based on the logical
    buses assigned to the channels of that mux. Once the link between
    MuxName, CHannelName, and logical bus is established, the system JSON is
    scanned to detect if there are any devices that have a "MuxChannel"
    object configuration that matches the MuxName and ChannelName and insert
    the corresponding logical bus to the system JSON so that it may be
    exposed in the device's entity-manager dbus object so it may be used by
    other daemons to create the sensors on the device. If the device whose
    logical bus was linked also happens to be a mux, a DeviceConfig object
    is created so this mux who now has a "Bus" field may be bound and link
    its channels and devices behind it in the same manner.
    
    Example:
    If the entity-manager configuration contains:
    {
      "Address": "0x71",
      "Bus": "5",
      "ChannelNames": [
          "ch0",
          "ch1",
          "ch2",
          "ch3"
      ],
      "Name": "MUX_1",
      "Type": "PCA9544Mux"
    },
    {
      "Address": "0x40",
      "MuxChannel":
      {
          "ChannelName": "ch0",
          "MuxName": "MUX_1"
      },
      "Name": "ADM1266_1",
      "Type": "ADM1266"
    }
    
    If MUX_1 happens to bind and create logical buses linking its channels
    to the following configuration:
    - ch0 <-> i2c-40
    - ch1 <-> i2c-41
    - ch2 <-> i2c-42
    - ch3 <-> i2c-43
    
    Then the system's configuration JSON seen in
    /var/configuration/system.json will update and reflect in the
    entity-manager objects to the following:
    
    {
      "Address": 113,
      "Bus": 5,
      "ChannelNames": [
          "ch0",
          "ch1",
          "ch2",
          "ch3"
      ],
      "Name": "MUX_1",
      "Type": "PCA9544Mux"
    },
    {
      "Address": 64,
      "Bus": 40,
      "MuxChannel":
      {
          "ChannelName": "ch0",
          "MuxName": "MUX_1"
      },
      "Name": "ADM1266_1",
      "Type": "ADM1266"
    }
    
    Change-Id: I8a93735ac2f36930d2b9a7ff9342b50e760a85c5
    Signed-off-by: Jonico Eustaquio <jonico.eustaquio@fii-na.com>

--- src/entity_manager/entity_manager.cpp
+++ src/entity_manager/entity_manager.cpp
@@ -453,7 +453,7 @@ void EntityManager::publishNewConfiguration(
     // NOLINTNEXTLINE(performance-unnecessary-value-param)
     const nlohmann::json newConfiguration)
 {
-    loadOverlays(newConfiguration, io);
+    loadOverlays(newConfiguration, systemConfiguration, io);
 
     boost::asio::post(io, [this]() {
         if (!writeJsonFiles(systemConfiguration))
--- src/entity_manager/overlay.hpp
+++ src/entity_manager/overlay.hpp
@@ -2,9 +2,111 @@
 // SPDX-FileCopyrightText: Copyright 2018 Intel Corporation
 
 #pragma once
-#include <boost/asio/io_context.hpp>
+
+#include "devices.hpp"
+
+#include <boost/asio/steady_timer.hpp>
+#include <boost/process/child.hpp>
 #include <nlohmann/json.hpp>
+
+#include <iostream>
+#include <regex>
+
+constexpr const char* outputDir = "/tmp/overlays";
+constexpr const char* templateChar = "$";
+constexpr const char* i2CDevsDir = "/sys/bus/i2c/devices";
+constexpr const char* muxSymlinkDir = "/dev/i2c-mux";
+const std::regex illegalNameRegex("[^A-Za-z0-9_]");
+
+class DeviceConfig
+{
+  public:
+    DeviceConfig() = delete;
+
+    explicit DeviceConfig(const std::string& type,
+                          const devices::ExportTemplate& exportTemplate,
+                          const nlohmann::json& configuration) :
+        type(type), parameters(exportTemplate.parameters),
+        busPath(exportTemplate.busPath), constructor(exportTemplate.add),
+        destructor(exportTemplate.remove),
+        hasHWMonDir(exportTemplate.hasHWMonDir)
+    {
+        for (auto keyPair = configuration.begin();
+             keyPair != configuration.end(); keyPair++)
+        {
+            std::string subsituteString;
+
+            if (keyPair.key() == "Name" &&
+                keyPair.value().type() == nlohmann::json::value_t::string)
+            {
+                subsituteString = std::regex_replace(
+                    keyPair.value().get<std::string>(), illegalNameRegex, "_");
+                name = subsituteString;
+            }
+            else
+            {
+                subsituteString = jsonToString(keyPair.value());
+            }
+
+            if (keyPair.key() == "Bus")
+            {
+                bus = keyPair.value().get<uint64_t>();
+            }
+            else if (keyPair.key() == "Address")
+            {
+                address = keyPair.value().get<uint64_t>();
+            }
+            else if (keyPair.key() == "ChannelNames" && type.ends_with("Mux"))
+            {
+                channels = keyPair.value().get<std::vector<std::string>>();
+            }
+            boost::replace_all(parameters, templateChar + keyPair.key(),
+                               subsituteString);
+            boost::replace_all(busPath, templateChar + keyPair.key(),
+                               subsituteString);
+        }
+    };
+
+    // helper function to make json types into string
+    static std::string jsonToString(const nlohmann::json& in)
+    {
+        if (in.type() == nlohmann::json::value_t::string)
+        {
+            return in.get<std::string>();
+        }
+        if (in.type() == nlohmann::json::value_t::array)
+        {
+            // remove brackets and comma from array
+            std::string array = in.dump();
+            array = array.substr(1, array.size() - 2);
+            boost::replace_all(array, ",", " ");
+            return array;
+        }
+        return in.dump();
+    }
+
+    bool buildDevice(const size_t retries = 5);
+    bool isMux();
+    std::vector<DeviceConfig> linkMux(nlohmann::json& systemConfiguration);
+
+  private:
+    bool deviceIsCreated();
+    bool createDevice();
+    bool deleteDevice();
+
+    std::string type;
+    std::string parameters;
+    std::string busPath;
+    std::string constructor;
+    std::string destructor;
+    devices::createsHWMon hasHWMonDir;
+    std::string name = "unknown";
+    std::optional<uint64_t> bus;
+    std::optional<uint64_t> address;
+    std::vector<std::string> channels;
+};
 
 void unloadAllOverlays();
 bool loadOverlays(const nlohmann::json& systemConfiguration,
+                  nlohmann::json& currConfiguration,
                   boost::asio::io_context& io);
