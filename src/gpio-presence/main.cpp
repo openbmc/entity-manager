8c4b1d999a13b27d6edc84cfaff1112a0cc07091
Alexander Hansen <alexander.hansen@9elements.com>
2024-11-04 14:06:24 +0100

    Add gpio-presence-sensor
    This is to implement the gpio based hw inventory design [1].
    
    There is a new meson option 'gpio-presence' to enable/disable the
    daemon.
    
    Summary of the functionality:
    
    - fetch configuration from EM, according to the configuration interface
    - the D-Bus interface is
      xyz.openbmc_project.Configuration.GPIODeviceDetect
    - the configuration represents devices for which presence can be
      detected based on gpio values.
    - watch gpios for changes
    - add/remove the xyz.openbmc_project.Inventory.Source.DevicePresence
      interface on the object path based on gpio values.
    
    References:
    [1] https://github.com/openbmc/docs/blob/master/designs/inventory/gpio-based-hardware-inventory.md
    [2] https://www.kernel.org/doc/html/latest/admin-guide/gpio/gpio-sim.html
    
    Tested: using linux gpio-sim facility, see below
    
    1. create a fake gpio via [2]
    2. configure gpio-presence-sensor as per [1]
    3. run the gpio-presence-sensor
    4. change the value of the gpio previously configured
    5. there should be log output (at debug level)
    6. the dbus interfaces exposed should appear/disappear as per [1]
    
    Change-Id: I4cf039b583247581aa5c6c6c59e7fc41ced0bb85
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ src/gpio-presence/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2022-2024.
+ * All rights reserved. SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "gpio_presence_manager.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async/context.hpp>
+
+using namespace gpio_presence;
+
+auto main() -> int
+{
+    lg2::debug("starting GPIO Presence Sensor");
+
+    sdbusplus::async::context ctx;
+
+    gpio_presence::GPIOPresenceManager controller(ctx);
+
+    controller.setupBusName();
+
+    controller.start();
+
+    ctx.run();
+}
