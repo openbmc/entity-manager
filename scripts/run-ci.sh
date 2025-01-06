1bfc16737768ad7434976490736660a2b62f81ad
Alexander Hansen <alexander.hansen@9elements.com>
2025-01-06 17:01:54 +0100

    meson: generate configurations list
    To avoid manually listing all the configuration files, use a script to
    make a list of them.
    
    There is a tradeoff when running a command to get the filenames, as it
    may not regenerate when file is added/removed [1].
    
    So the list can still be kept as source, but generated in its own
    separate file.
    
    References:
    [1] https://mesonbuild.com/FAQ.html#but-i-really-want-to-use-wildcards
    
    Change-Id: I760486bafe94fce6485dfb7627946d0a77fdd322
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- scripts/run-ci.sh
+++ scripts/run-ci.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+set -e
+
 scripts/validate_configs.py -v -k -e test/expected-schema-errors.txt
+
+# fail the script if someone forgot to update the list of configurations
+cp configurations/meson.build /tmp/configurations_meson.txt
+scripts/generate_config_list.sh
+diff /tmp/configurations_meson.txt configurations/meson.build
