1612d102e02bca80a8a5ed54473e5fdfd0344285
Alexander Hansen <alexander.hansen@9elements.com>
2025-01-27 17:23:46 +0100

    meson: option 'compatible-hardware' to customize
    To improve BMC responsiveness and speed up configuration parsing on
    first boot after fw update (EM configuration is not yet cached), the
    configuration files which are installed can now be decided by a list of
    compatible hardware to support.
    
    This also helps to have a better overview about which configurations are
    present and grep more quickly.
    
    The default value for the meson option should preserve the previous
    behavior of installing all configurations.
    
    Example values for the meson option 'compatible-hardware':
    
    "tyan-s5549 tyan-s8030 tyan-s8040 tyan-s8047"
    "meta-harma meta-yv4"
    "ibm-sbp1"
    
    Configurations which apply to many boards, such as PSU configuration,
    are installed by default since they have
    
    ```
    "CompatibleHardware": "*"
    ```
    
    Benchmark on Tyan S8030 with AST2500 chip:
    
    ```
    Mar 05 10:55:26 s8030-bmc systemd[1]: Started Entity Manager.
    Mar 05 10:55:42 s8030-bmc entity-manager[4974]: Found probe match on /xyz/openbmc_project/FruDevice/S8030GM2NE xyz.openbmc_project.FruDevice
    ```
    6 seconds until probe match after rm /var/configuration/system.json and
    restart.
    
    ```
    Mar 05 11:04:30 s8030-bmc systemd[1]: Started Entity Manager.
    Mar 05 11:04:46 s8030-bmc entity-manager[5562]: Found probe match on /xyz/openbmc_project/FruDevice/S8030GM2NE xyz.openbmc_project.FruDevice
    ```
    
    again, 6 seconds until probe match after
    rm /var/configuration/system.json and restart
    
    Then, after removing the unused configurations, the first probe match
    occurs after 3 seconds.
    
    ```
    Mar 05 11:12:15 s8030-bmc systemd[1]: Started Entity Manager.
    Mar 05 11:12:18 s8030-bmc entity-manager[6077]: Found probe match on /xyz/openbmc_project/FruDevice/S8030GM2NE xyz.openbmc_project.FruDevice
    ```
    And repeating the benchmark:
    
    ```
    Mar 05 11:13:21 s8030-bmc systemd[1]: Started Entity Manager.
    Mar 05 11:13:24 s8030-bmc entity-manager[6171]: Found probe match on /xyz/openbmc_project/FruDevice/S8030GM2NE xyz.openbmc_project.FruDevice
    ```
    
    So the  improvement is around 3 seconds, even while still keeping 15
    json configurations in /usr/share/entity-manager/configurations, not
    all of which are actually needed for the board.
    
    Tested: below
    
    ```
    meson configure -D compatible-hardware="tyan-s8030 tyan-s8040" build/
    cd build/
    meson install --destdir /tmp/em-meson-install/
    ```
    
    confirm that only the tyan configs are installed.
    
    ```
    meson configure -D compatible-hardware="" build/
    cd build/
    meson install --destdir /tmp/em-meson-install/
    ```
    
    confirm that all the configs are installed as before.
    
    Change-Id: I8a8c92f8fd0d6d4fcba1e025c5312e7d211888d7
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ scripts/filter_configs.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+
+
+def filter_config(compatible_hw, config):
+
+    if config.endswith(".record.json"):
+        return True
+
+    try:
+        with open(config) as fd:
+            j = json.load(fd)
+    except Exception:
+        return False
+
+    if isinstance(j, dict):
+        if "CompatibleHardware" in j:
+            comp_hw = j["CompatibleHardware"]
+            if comp_hw == "":
+                return True
+            elif comp_hw in compatible_hw:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    return True
+
+
+def filter_configs(compatible_hw, configs):
+
+    if len(compatible_hw) == 0:
+        return configs
+
+    filtered_configs = []
+
+    for config in configs:
+        if filter_config(compatible_hw, config):
+            filtered_configs.append(config)
+
+    return filtered_configs
+
+
+# Parse arguments
+parser = argparse.ArgumentParser(
+    description="Filter config files based on 'CompatibleHardware' property"
+)
+parser.add_argument(
+    "--compatible-hardware",
+    required=True,
+    help="space separated list of hw to support, use empty list for all hw",
+)
+parser.add_argument(
+    "--configs", required=True, help="Space-separated list of config files."
+)
+args = parser.parse_args()
+
+configs = args.configs.split()
+
+filtered_configs = filter_configs(
+    args.compatible_hardware.strip().split(), configs
+)
+
+# Print filtered configs (one per line)
+for config in filtered_configs:
+    print(config)
