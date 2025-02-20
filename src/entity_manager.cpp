openbmc/entity-manager

commit 5c1a61a84069b48ccc27e45ffc82d5ca87b330a1

Fix threshold probability loss
During bmc reset stress testing, some sensors occasionally lose
thresholds, and the lost thresholds are not created on DBus.

This is because when EM loads the json file and creates interface,
dbus-sensors may have listened and started to create the sensor just
after the `Type` interface is created but before the threshold
interface is created. This is the sensor interface obtained from EM
and does not include the threshold interface. Therefore, the sensor
displayed on DBus does not have a threshold interface.

This commit changes the order of creating interfaces in EM. The Type
interface should be created after the Threshold interface is created.
This is because for the callback method of dbus-sensors, when the
Type interface is created, it is considered that all configurations
of this sensor have been loaded.

Tested:
The bmc reset stress test did not reproduce this issue.

Signed-off-by: George Liu <liuxiwei@ieisystem.com>
Change-Id: Ia8d9e24a8697992cab13aecb35438f142ed29e67

src/entity_manager.cpp
@@ -700,11 +700,6 @@ void postToDbus(const nlohmann::json& newConfiguration,
             ifacePath += "/";
             ifacePath += itemName;
 
-            std::shared_ptr<sdbusplus::asio::dbus_interface> itemIface =
-                createInterface(objServer, ifacePath,
-                                "xyz.openbmc_project.Configuration." + itemType,
-                                boardNameOrig);
-
             if (itemType == "BMC")
             {
                 std::shared_ptr<sdbusplus::asio::dbus_interface> bmcIface =
@@ -725,10 +720,6 @@ void postToDbus(const nlohmann::json& newConfiguration,
                                           systemIface, item, objServer,
                                           getPermission(itemType));
             }
-
-            populateInterfaceFromJson(systemConfiguration, jsonPointerPath,
-                                      itemIface, item, objServer,
-                                      getPermission(itemType));
 
             for (const auto& [name, config] : item.items())
             {
@@ -799,6 +790,15 @@ void postToDbus(const nlohmann::json& newConfiguration,
                     }
                 }
             }
+
+            std::shared_ptr<sdbusplus::asio::dbus_interface> itemIface =
+                createInterface(objServer, ifacePath,
+                                "xyz.openbmc_project.Configuration." + itemType,
+                                boardNameOrig);
+
+            populateInterfaceFromJson(systemConfiguration, jsonPointerPath,
+                                      itemIface, item, objServer,
+                                      getPermission(itemType));
 
             topology.addBoard(boardPath, boardType, boardNameOrig, item);
         }

commit 015e748ad97a8a3d5057b54f00f763909c6f2510

fix the json pointer path error
fixes:  5c1a61a (Fix threshold probability loss)

After moving the `populateInterfaceFromJson` function to the end of the
loop, the `jsonPointerPath` is modified within the loop
(`for (const auto& [name, config] : item.items())`), causing a json
pointer path error and failure to set the property."

So rename some variables to make the code more readable and fix the
json pointer path error.

Tested: no error and set property success.
~# busctl set-property xyz.openbmc_project.EntityManager \
    /xyz/openbmc_project/inventory/system/board/Foo/Item1 \
    xyz.openbmc_project.Configuration.Item1 \
    Property1 s "test"

Change-Id: Ic46378c7635a27a6df39419507ba5f9f6b270ab6
Signed-off-by: Jian Zhang <zhangjian.3032@bytedance.com>

src/entity_manager.cpp
@@ -663,8 +663,8 @@ void postToDbus(const nlohmann::json& newConfiguration,
         for (auto& item : *exposes)
         {
             exposesIndex++;
-            jsonPointerPath = jsonPointerPathBoard;
-            jsonPointerPath += std::to_string(exposesIndex);
+            auto exposesIndexJsonPointer =
+                jsonPointerPathBoard + std::to_string(exposesIndex);
 
             auto findName = item.find("Name");
             if (findName == item.end())
@@ -723,10 +723,10 @@ void postToDbus(const nlohmann::json& newConfiguration,
 
             for (const auto& [name, config] : item.items())
             {
-                jsonPointerPath = jsonPointerPathBoard;
-                jsonPointerPath.append(std::to_string(exposesIndex))
-                    .append("/")
-                    .append(name);
+                auto objOrArrayBaseJsonPointer = exposesIndexJsonPointer;
+                objOrArrayBaseJsonPointer += "/";
+                objOrArrayBaseJsonPointer += name;
+
                 if (config.type() == nlohmann::json::value_t::object)
                 {
                     std::string ifaceName =
@@ -738,8 +738,8 @@ void postToDbus(const nlohmann::json& newConfiguration,
                                                       ifaceName, boardNameOrig);
 
                     populateInterfaceFromJson(
-                        systemConfiguration, jsonPointerPath, objectIface,
-                        config, objServer, getPermission(name));
+                        systemConfiguration, objOrArrayBaseJsonPointer,
+                        objectIface, config, objServer, getPermission(name));
                 }
                 else if (config.type() == nlohmann::json::value_t::array)
                 {
@@ -783,7 +783,8 @@ void postToDbus(const nlohmann::json& newConfiguration,
 
                         populateInterfaceFromJson(
                             systemConfiguration,
-                            jsonPointerPath + "/" + std::to_string(index),
+                            objOrArrayBaseJsonPointer + "/" +
+                                std::to_string(index),
                             objectIface, arrayItem, objServer,
                             getPermission(name));
                         index++;
@@ -796,9 +797,9 @@ void postToDbus(const nlohmann::json& newConfiguration,
                                 "xyz.openbmc_project.Configuration." + itemType,
                                 boardNameOrig);
 
-            populateInterfaceFromJson(systemConfiguration, jsonPointerPath,
-                                      itemIface, item, objServer,
-                                      getPermission(itemType));
+            populateInterfaceFromJson(systemConfiguration,
+                                      exposesIndexJsonPointer, itemIface, item,
+                                      objServer, getPermission(itemType));
 
             topology.addBoard(boardPath, boardType, boardNameOrig, item);
         }