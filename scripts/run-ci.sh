698375de3426fc30329c5678741b85a20e96428a
Alexander Hansen <alexander.hansen@9elements.com>
2025-04-25 18:33:44 +0200

    scripts: auto-generate list of schemas
    Since the list of schemas is growing and we already have a directory, it
    does not need to be updated manually. Make a script for it and check
    that that's been done in CI.
    
    Tested: Script works and updates the files as expected.
    
    Change-Id: Ia1b3d99890477c4077e8e3a52a5338805d810440
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- scripts/run-ci.sh
+++ scripts/run-ci.sh
@@ -7,6 +7,7 @@ scripts/validate_configs.py -v -k -e test/expected-schema-errors.txt
 # fail the script if someone forgot to update configurations from templates
 scripts/generate_configs.sh
 
-# fail the script if someone forgot to update the list of configurations
+# fail the script if someone forgot to update the list of configurations or schemas
 scripts/generate_config_list.sh
+scripts/generate_schema_list.sh
 git --no-pager diff --exit-code -- .
