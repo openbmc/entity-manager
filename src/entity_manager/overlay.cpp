c2032cb053b70ac2667a102b46c8cd3b8e859c7c
Donald Shannon <donalds@nvidia.com>
2025-05-18 15:43:12 -0700

    entity_manager: Added GPIO expanders to device overlay
    Creates gpiochip at the I2C location specified in the configs
    Added nvidia_gb200_verona config that demo-es this feature
    
    Change-Id: I70380fef75ed317b0011cc7f2224f04272cbe188
    Signed-off-by: Donald Shannon <donalds@nvidia.com>

--- src/entity_manager/overlay.cpp
+++ src/entity_manager/overlay.cpp
@@ -40,7 +40,6 @@ constexpr const char* outputDir = "/tmp/overlays";
 constexpr const char* templateChar = "$";
 constexpr const char* i2CDevsDir = "/sys/bus/i2c/devices";
 constexpr const char* muxSymlinkDir = "/dev/i2c-mux";
-
 const std::regex illegalNameRegex("[^A-Za-z0-9_]");
 
 // helper function to make json types into string
@@ -169,7 +168,8 @@ static int buildDevice(
     const std::string& parameters, uint64_t bus, uint64_t address,
     const std::string& constructor, const std::string& destructor,
     const devices::createsHWMon hasHWMonDir,
-    std::vector<std::string> channelNames, const size_t retries = 5)
+    std::vector<std::string> channelNames, [[maybe_unused]] std::vector<std::string> gpios,
+    const size_t retries = 5)
 {
     if (retries == 0U)
     {
@@ -230,6 +230,7 @@ void exportDevice(const std::string& type,
     std::optional<uint64_t> bus;
     std::optional<uint64_t> address;
     std::vector<std::string> channels;
+    std::vector<std::string> gpios;
 
     for (auto keyPair = configuration.begin(); keyPair != configuration.end();
          keyPair++)
@@ -260,6 +261,10 @@ void exportDevice(const std::string& type,
         {
             channels = keyPair.value().get<std::vector<std::string>>();
         }
+        else if (keyPair.key() == "GpioNames" && type.ends_with("Iox"))
+        {
+            gpios = keyPair.value().get<std::vector<std::string>>();
+        }
         boost::replace_all(parameters, templateChar + keyPair.key(),
                            subsituteString);
         boost::replace_all(busPath, templateChar + keyPair.key(),
@@ -273,7 +278,7 @@ void exportDevice(const std::string& type,
     }
 
     buildDevice(name, busPath, parameters, *bus, *address, constructor,
-                destructor, hasHWMonDir, std::move(channels));
+                destructor, hasHWMonDir, std::move(channels), std::move(gpios));
 }
 
 bool loadOverlays(const nlohmann::json& systemConfiguration)
