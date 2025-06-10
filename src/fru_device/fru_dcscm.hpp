d3ae0a12b95418b15d72bcc58f2bc3681f958f48
Jitendra Tripathy <jitendra.kumarx.tripathy@intel.com>
2025-06-10 09:03:02 +0000

    Expose Multi-record data on D-bus
    Current implementation does not show multi-record data
    on D-bus. It only displays board, chassis and product areas.
    
    So, implemented code to expose the multi-record fru data on
    D-bus via fru-device service. Followed the Datacenter Secure
    Control Module Specification(OCP DC-SCM Rev 2.1 Ver1.1)
    for the implementation.
    
    Tested:
    busctl introspect xyz.openbmc_project.FruDevice
        /xyz/openbmc_project/FruDevice/AAA_BBB_CCC
    NAME TYPE SIGNATURE RESULT/VALUE FLAGS
    ....
    .MULTIRECORD_DC_SCI_REVISION      property  s "2.1"      emits-change
    .MULTIRECORD_DC_SCI_VERSION       property  s "1.1"      emits-change
    .MULTIRECORD_DC_SCM_TYPE          property  s "1"        emits-change
    .MULTIRECORD_HPM_DEVICE_ID        property  s "0xff00"   emits-change
    .MULTIRECORD_HPM_VENDOR_ID        property  s "0x1234"   emits-change
    .MULTIRECORD_LTPI_REVISION        property  s "1.1"      emits-change
    .MULTIRECORD_LTPI_VERSION         property  s "1.1"      emits-change
    .MULTIRECORD_MANUFACTURE_ID       property  s "0x001234" emits-change
    .MULTIRECORD_OEM_RECORD_VERSION   property  s "1"        emits-change
    ....
    
    Change-Id: I26d3d52cd823a4d210672a83439c1b075b1cb7f0
    Signed-off-by: Jitendra Tripathy <jitendra.kumarx.tripathy@intel.com>

---
+++ src/fru_device/fru_dcscm.hpp
@@ -0,0 +1,30 @@
+/*
+// Copyright (c) 2025 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+/// \file fru_dcscm.hpp
+
+#pragma once
+#include "fru_utils.hpp"
+
+#include <string>
+#include <vector>
+
+/*This table describes HPM FRU MultiRecord fields and sizes
+ *as per OCP DC-SCM Rev 2.1 Ver1.1.
+ */
+const std::vector<std::pair<std::string, uint8_t>> hpmFruMuitiRecordTable = {
+    {"MANUFACTURE_ID", 3}, {"OEM_RECORD_VERSION", 1}, {"DC_SCI_REVISION", 2},
+    {"DC_SCI_VERSION", 1}, {"LTPI_REVISION", 2},      {"LTPI_VERSION", 1},
+    {"DC_SCM_TYPE", 1},    {"HPM_VENDOR_ID", 2},      {"HPM_DEVICE_ID", 2}};
