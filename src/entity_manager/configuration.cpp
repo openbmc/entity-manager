5931591fdd6ed6eb22d57bbe9e6d450476cb476c
Alexander Hansen <alexander.hansen@9elements.com>
2025-06-25 12:26:09 +0200

    entity-manager: runtime schema validation
    Uncomment the schema validation code and create a meson option to enable
    this code path. The option is by default false, so the default codepath
    is untouched.
    
    Tested: Not tested, since the schemas can be validated at build time.
    Whoever wants to experiment with this code, feel free to test it.
    At least now it is as easy as enabling a meson option.
    
    Change-Id: Icc2226e2e08ede5cff5a5f97bc29c8e38b117ff8
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- src/entity_manager/configuration.cpp
+++ src/entity_manager/configuration.cpp
@@ -83,15 +83,12 @@ bool loadConfigurations(std::list<nlohmann::json>& configurations)
             std::cerr << "syntax error in " << jsonPath.string() << "\n";
             continue;
         }
-        /*
-        * todo(james): reenable this once less things are in flight
-        *
-        if (!validateJson(schema, data))
+
+        if (ENABLE_RUNTIME_VALIDATE_JSON && !validateJson(schema, data))
         {
             std::cerr << "Error validating " << jsonPath.string() << "\n";
             continue;
         }
-        */
 
         if (data.type() == nlohmann::json::value_t::array)
         {
