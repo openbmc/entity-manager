5615ee4927cf1c102e01024990f472528026da8c
Alexander Hansen <alexander.hansen@9elements.com>
2025-06-27 15:07:07 +0200

    entity-manager: move logDeviceAdded/Removed
    functions do not need to be 'inline' and since they are normal
    functions, they can go into a .cpp file to be compiled separately.
    
    Functions otherwise unchanged.
    
    Tested: Code is almost unchanged. See no meaning of 'inline' [1] that
    is used here.
    
    References:
    [1] https://en.cppreference.com/w/cpp/language/inline.html
    
    Change-Id: I194d86f17f90d54d6be2286a52482457435f4c36
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- src/entity_manager/entity_manager.hpp
+++ src/entity_manager/entity_manager.hpp
@@ -17,12 +17,9 @@
 
 #pragma once
 
-#include "../utils.hpp"
 #include "dbus_interface.hpp"
 #include "topology.hpp"
 #include "utils.hpp"
-
-#include <systemd/sd-journal.h>
 
 #include <boost/container/flat_map.hpp>
 #include <nlohmann/json.hpp>
@@ -81,111 +78,3 @@ class EntityManager
     void startRemovedTimer(boost::asio::steady_timer& timer,
                            nlohmann::json& systemConfiguration);
 };
-
-inline void logDeviceAdded(const nlohmann::json& record)
-{
-    if (!deviceHasLogging(record))
-    {
-        return;
-    }
-    auto findType = record.find("Type");
-    auto findAsset =
-        record.find("xyz.openbmc_project.Inventory.Decorator.Asset");
-
-    std::string model = "Unknown";
-    std::string type = "Unknown";
-    std::string sn = "Unknown";
-    std::string name = "Unknown";
-
-    if (findType != record.end())
-    {
-        type = findType->get<std::string>();
-    }
-    if (findAsset != record.end())
-    {
-        auto findModel = findAsset->find("Model");
-        auto findSn = findAsset->find("SerialNumber");
-        if (findModel != findAsset->end())
-        {
-            model = findModel->get<std::string>();
-        }
-        if (findSn != findAsset->end())
-        {
-            const std::string* getSn = findSn->get_ptr<const std::string*>();
-            if (getSn != nullptr)
-            {
-                sn = *getSn;
-            }
-            else
-            {
-                sn = findSn->dump();
-            }
-        }
-    }
-
-    auto findName = record.find("Name");
-    if (findName != record.end())
-    {
-        name = findName->get<std::string>();
-    }
-
-    sd_journal_send("MESSAGE=Inventory Added: %s", name.c_str(), "PRIORITY=%i",
-                    LOG_INFO, "REDFISH_MESSAGE_ID=%s",
-                    "OpenBMC.0.1.InventoryAdded",
-                    "REDFISH_MESSAGE_ARGS=%s,%s,%s", model.c_str(),
-                    type.c_str(), sn.c_str(), "NAME=%s", name.c_str(), NULL);
-}
-
-inline void logDeviceRemoved(const nlohmann::json& record)
-{
-    if (!deviceHasLogging(record))
-    {
-        return;
-    }
-    auto findType = record.find("Type");
-    auto findAsset =
-        record.find("xyz.openbmc_project.Inventory.Decorator.Asset");
-
-    std::string model = "Unknown";
-    std::string type = "Unknown";
-    std::string sn = "Unknown";
-    std::string name = "Unknown";
-
-    if (findType != record.end())
-    {
-        type = findType->get<std::string>();
-    }
-    if (findAsset != record.end())
-    {
-        auto findModel = findAsset->find("Model");
-        auto findSn = findAsset->find("SerialNumber");
-        if (findModel != findAsset->end())
-        {
-            model = findModel->get<std::string>();
-        }
-        if (findSn != findAsset->end())
-        {
-            const std::string* getSn = findSn->get_ptr<const std::string*>();
-            if (getSn != nullptr)
-            {
-                sn = *getSn;
-            }
-            else
-            {
-                sn = findSn->dump();
-            }
-        }
-    }
-
-    auto findName = record.find("Name");
-    if (findName != record.end())
-    {
-        name = findName->get<std::string>();
-    }
-
-    sd_journal_send("MESSAGE=Inventory Removed: %s", name.c_str(),
-                    "PRIORITY=%i", LOG_INFO, "REDFISH_MESSAGE_ID=%s",
-                    "OpenBMC.0.1.InventoryRemoved",
-                    "REDFISH_MESSAGE_ARGS=%s,%s,%s", model.c_str(),
-                    type.c_str(), sn.c_str(), "NAME=%s", name.c_str(), NULL);
-}
