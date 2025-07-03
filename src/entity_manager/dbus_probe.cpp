33eefcddbe846e6365a172c90e565411802b28d1
Christopher Meis <christopher.meis@9elements.com>
2025-07-03 13:26:18 +0200

    Add class DbusProbe
    This class reduces iteration of configurations and holds information
    about probes.
    
    Change-Id: Ibb79c027656581f8f10da40b9e96fdea05723f70
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

---
+++ src/entity_manager/dbus_probe.cpp
@@ -0,0 +1,6 @@
+#include "dbus_probe.hpp"
+
+DbusProbe::DbusProbe(std::string* name, nlohmann::json* command,
+                     nlohmann::json& ref, std::set<std::string>* interface) :
+    name(*name), command(*command), ref(&ref), interface(*interface)
+{}
