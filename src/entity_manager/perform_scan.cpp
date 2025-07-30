b48aad5970e36f86a5c51014662558be03df75db
Christopher Meis <christopher.meis@9elements.com>
2025-07-30 10:38:37 +0200

    entity-manager: Add class ObjectMapper
    This class is responsible for interaction with the ObjectMapper on Dbus.
    It separates the code further, increasing maintainability.
    
    The following diagram shows the integration into the software design:
    
    ```
    ┌──────────────────────────────────────────────────────────────────────────┐
    │┌──────────────┐                                                          │
    ││┌──────────────┐                                                         │
    │└┼┬──────────────┐                                                        │
    │ └┼ Json Configs │                                                        │
    │  └──────┬───────┘                                                        │
    │         │                  ┌─────────────────┐                           │
    │         ▼                  │  EntityManager  │                           │
    │         │                  │     <class>     │                           │
    │         ▼                  └───────▲┌────────┘                           │
    │         │                          ││                                    │
    │         ▼                          │▼                                    │
    │         │                          ▲│                                    │
    │┌────────▼────────────┐     ┌───────┘▼────────┐     ┌───────────────────┐ │
    ││ ConfigurationManager│◄──◄─┼   SystemMapper  ┼──►──►  InventoryManager │ │
    ││       <class>       ┼─►──►│     <class>     │◄──◄─┐      <class>      │ │
    │└─────────────────────┘     └──────┬─▲────────┘     └──────────┬────────┘ │
    │                                   ▼ │                         ▼          │
    ├── ──  ── ─── ─── ─── ───          │ ▲                         │          │
    │                         │         ▼ │              ── ─── ────▼── ── ────│
    │   ┌─────────────────┐             │ ▲             │           │          │
    │   │  EntityManager  │   │  ┌──────▼─┴────────┐    │  ┌────────▼────────┐ │
    │   │       on        ◄──◄┤◄─┼  ObjectMapper   │       │  EntityManager  │ │
    │   │      DBUS       ├─►─┼►─►     <class>     │    │  │       on        │ │
    │   └─────────────────┘   │  └─────────────────┘    │  │      DBUS       │ │
    │                           ────   ────   ────  ───    └─────────────────┘ │
    │                                                                          │
    └──────────────────────────────────────────────────────────────────────────┘
    ```
    
    Tested: not yet
    
    Change-Id: I51d7e70bdce132cd7d0543cda92646275bf72267
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- src/entity_manager/perform_scan.cpp
+++ src/entity_manager/perform_scan.cpp
@@ -16,6 +16,7 @@
 /// \file perform_scan.cpp
 #include "perform_scan.hpp"
 
+#include "object_mapper.hpp"
 #include "perform_probe.hpp"
 #include "system_mapper.hpp"
 #include "utils.hpp"
@@ -468,10 +469,24 @@ void scan::PerformScan::run()
         it++;
     }
 
+    // Filter out interfaces already obtained.
+    for (const auto& [path, probeInterfaces] :
+         _em.systemMapper->dbusProbeObjects)
+    {
+        for (const auto& [interface, _] : probeInterfaces)
+        {
+            dbusProbeInterfaces.erase(interface);
+        }
+    }
+    if (dbusProbeInterfaces.empty())
+    {
+        return;
+    }
+
     // probe vector stores a shared_ptr to each PerformProbe that cares
     // about a dbus interface
-    _em.systemMapper->findDbusObjects(std::move(dbusProbePointers),
-                                      std::move(dbusProbeInterfaces));
+    _em.systemMapper->objectMapper.getSubTree(std::move(dbusProbePointers),
+                                              std::move(dbusProbeInterfaces));
 }
 
 scan::PerformScan::~PerformScan()
