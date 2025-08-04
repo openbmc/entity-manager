30858db25f5ac7cfd6662b2e80c9113cb12c197b
Alexander Hansen <alexander.hansen@9elements.com>
2025-08-04 16:27:10 +0200

    test: entity-manager: dbus interface
    Write tests for the EM DBus interfaces.
    
    Given a piece of json configuration record, call the appropriate
    functions to expose it on DBus.
    
    Then make dbus calls to query the resulting DBus objects.
    
    Tested: Unit Tests Pass.
    
    Change-Id: I67acef6ecde05c18e99221c4a148fa565e9d9463
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ test/entity_manager/dbus_test.hpp
@@ -0,0 +1,83 @@
+#pragma once
+
+#include "test_em.hpp"
+#include "utils.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+using namespace std::string_literals;
+
+class DBusTest : public testing::Test
+{
+  protected:
+    DBusTest() :
+        systemBus(std::make_shared<sdbusplus::asio::connection>(io)),
+        em(systemBus, io)
+    {}
+    ~DBusTest() noexcept override = default;
+
+    boost::asio::io_context io;
+    std::shared_ptr<sdbusplus::asio::connection> systemBus;
+    TestEM em;
+
+  public:
+    DBusTest(const DBusTest&) = delete;
+    DBusTest(DBusTest&&) = delete;
+    DBusTest& operator=(const DBusTest&) = delete;
+    DBusTest& operator=(DBusTest&&) = delete;
+
+    void postAssertHandlerCallback(
+        const std::function<void(const DBusInterface& value)>& handler,
+        bool stopOnError, const boost::system::error_code& ec,
+        const DBusInterface& value)
+    {
+        EXPECT_FALSE(ec);
+        if (ec)
+        {
+            lg2::error("{ERROR}", "ERROR", ec.message());
+
+            // not run below line to manually inspect DBus in case of test
+            // failure
+            if (stopOnError)
+            {
+                io.stop();
+            }
+
+            return;
+        }
+
+        try
+        {
+            handler(value);
+            io.stop();
+        }
+        catch (std::exception& e)
+        {
+            if (stopOnError)
+            {
+                io.stop();
+            }
+        }
+    };
+
+    void postAssertHandler(
+        const std::function<void(const DBusInterface& value)>& handler,
+        const std::string& path, const std::string& interface,
+        bool stopOnError = true)
+    {
+        std::function<void(const boost::system::error_code&,
+                           const DBusInterface&)>
+            h3 = std::bind_front(&DBusTest::postAssertHandlerCallback, this,
+                                 handler, stopOnError);
+
+        systemBus->async_method_call(h3, em.busName, path,
+                                     "org.freedesktop.DBus.Properties",
+                                     "GetAll", interface);
+
+        io.run();
+    };
+};
---
+++ test/entity_manager/test_dbus_interface.cpp
@@ -0,0 +1,217 @@
+#include "dbus_test.hpp"
+#include "entity_manager/dbus_interface.hpp"
+#include "test_em.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+const std::string configInterface = "xyz.openbmc_project.Configuration.Example";
+
+const std::string objPath =
+    "/xyz/openbmc_project/inventory/system/board/MyBoard/MyRecord";
+
+const std::string jsonPointerPath = "xyz";
+
+TEST_F(DBusTest, createInterface)
+{
+    const std::string path = "/xyz/openbmc_project/inventory/mypath";
+    std::shared_ptr<sdbusplus::asio::dbus_interface> iface =
+        em.em.dbus_interface.createInterface(path, configInterface, "myboard");
+
+    dbus_interface::tryIfaceInitialize(iface);
+
+    auto handler = [](const DBusInterface& value) {
+        // no properties on this interface
+        EXPECT_EQ(value.size(), 0);
+    };
+
+    postAssertHandler(handler, path, configInterface);
+}
+
+TEST_F(DBusTest, populateInterfaceFromJson_Int)
+{
+    nlohmann::json dict = nlohmann::json::parse(R"(
+{
+    "Name": "MyRecord",
+    "X1": 3,
+    "X2": -3,
+    "Type": "Example"
+}
+    )");
+
+    std::shared_ptr<sdbusplus::asio::dbus_interface> iface =
+        em.em.dbus_interface.createInterface(objPath, configInterface,
+                                             "myboard");
+
+    nlohmann::json systemConfiguration = "{}";
+    em.em.dbus_interface.populateInterfaceFromJson(
+        systemConfiguration, jsonPointerPath, iface, dict);
+
+    auto handler = [](const DBusInterface& value) {
+        EXPECT_EQ(value.size(), 4);
+
+        auto name = std::get<std::string>(std::get<1>(*value.find("Name")));
+        EXPECT_EQ(name, "MyRecord");
+
+        auto type = std::get<std::string>(std::get<1>(*value.find("Type")));
+        EXPECT_EQ(type, "Example");
+
+        EXPECT_EQ(std::get<int64_t>(std::get<1>(*value.find("X1"))), 3);
+        EXPECT_EQ(std::get<int64_t>(std::get<1>(*value.find("X2"))), -3);
+
+        EXPECT_EQ(value.find("Y"), value.end());
+    };
+    postAssertHandler(handler, objPath, configInterface);
+}
+
+TEST_F(DBusTest, populateInterfaceFromJson_Double)
+{
+    nlohmann::json dict = nlohmann::json::parse(R"(
+{
+    "Name": "MyRecord",
+    "PropertyDouble": 2.0,
+    "Type": "Example"
+}
+    )");
+
+    std::shared_ptr<sdbusplus::asio::dbus_interface> iface =
+        em.em.dbus_interface.createInterface(objPath, configInterface,
+                                             "myboard");
+
+    nlohmann::json systemConfiguration = "{}";
+    em.em.dbus_interface.populateInterfaceFromJson(
+        systemConfiguration, jsonPointerPath, iface, dict);
+
+    auto handler = [](const DBusInterface& value) {
+        EXPECT_EQ(value.size(), 3);
+
+        EXPECT_EQ(std::get<double>(std::get<1>(*value.find("PropertyDouble"))),
+                  2.0);
+    };
+    postAssertHandler(handler, objPath, configInterface);
+}
+
+TEST_F(DBusTest, populateInterfaceFromJson_Bool)
+{
+    nlohmann::json dict = nlohmann::json::parse(R"(
+{
+    "Name": "MyRecord",
+    "BoolProp": true,
+    "Type": "Example"
+}
+    )");
+
+    std::shared_ptr<sdbusplus::asio::dbus_interface> iface =
+        em.em.dbus_interface.createInterface(objPath, configInterface,
+                                             "myboard");
+
+    nlohmann::json systemConfiguration = "{}";
+    em.em.dbus_interface.populateInterfaceFromJson(
+        systemConfiguration, jsonPointerPath, iface, dict);
+
+    auto handler = [](const DBusInterface& value) {
+        EXPECT_EQ(value.size(), 3);
+
+        auto b = std::get<bool>(std::get<1>(*value.find("BoolProp")));
+        EXPECT_EQ(b, true);
+    };
+    postAssertHandler(handler, objPath, configInterface);
+}
+
+TEST_F(DBusTest, populateInterfaceFromJson_Arrays_Int)
+{
+    nlohmann::json dict = nlohmann::json::parse(R"(
+{
+    "Name": "MyRecord",
+    "ArrayOfInts": [3,2,1],
+    "Type": "Example"
+}
+)");
+
+    std::shared_ptr<sdbusplus::asio::dbus_interface> iface =
+        em.em.dbus_interface.createInterface(objPath, configInterface,
+                                             "myboard");
+
+    nlohmann::json systemConfiguration = "{}";
+    em.em.dbus_interface.populateInterfaceFromJson(
+        systemConfiguration, jsonPointerPath, iface, dict);
+
+    auto handler = [](const DBusInterface& value) {
+        EXPECT_EQ(value.size(), 3);
+
+        auto x = std::get<std::vector<int64_t>>(
+            std::get<1>(*value.find("ArrayOfInts")));
+        EXPECT_EQ(x.size(), 3);
+        EXPECT_EQ(x[1], 2);
+    };
+    postAssertHandler(handler, objPath, configInterface);
+}
+
+TEST_F(DBusTest, populateInterfaceFromJson_Arrays_Double)
+{
+    nlohmann::json dict = nlohmann::json::parse(R"(
+{
+    "Name": "MyRecord",
+    "ArrayOfDouble": [1.3, 1.6, 1.8],
+    "Type": "Example"
+}
+)");
+
+    std::shared_ptr<sdbusplus::asio::dbus_interface> iface =
+        em.em.dbus_interface.createInterface(objPath, configInterface,
+                                             "myboard");
+
+    nlohmann::json systemConfiguration = "{}";
+    em.em.dbus_interface.populateInterfaceFromJson(
+        systemConfiguration, jsonPointerPath, iface, dict);
+
+    auto handler = [](const DBusInterface& value) {
+        EXPECT_EQ(value.size(), 3);
+
+        EXPECT_TRUE(value.contains("ArrayOfDouble"));
+
+        auto val = *value.find("ArrayOfDouble");
+
+        EXPECT_TRUE(
+            std::holds_alternative<std::vector<double>>(std::get<1>(val)));
+
+        // EM quirk: array of bools gets exposed as array of uint64_t
+        auto b = std::get<std::vector<double>>(std::get<1>(val));
+
+        EXPECT_EQ(b.size(), 3);
+        EXPECT_EQ(b[1], 1.6);
+    };
+    postAssertHandler(handler, objPath, configInterface);
+}
+
+TEST_F(DBusTest, populateInterfaceFromJson_Arrays_String)
+{
+    nlohmann::json dict = nlohmann::json::parse(R"(
+{
+    "Name": "MyRecord",
+    "ArrayOfStrings": ["string1", "string2", "string3"],
+    "Type": "Example"
+}
+)");
+
+    std::shared_ptr<sdbusplus::asio::dbus_interface> iface =
+        em.em.dbus_interface.createInterface(objPath, configInterface,
+                                             "myboard");
+
+    nlohmann::json systemConfiguration = "{}";
+    em.em.dbus_interface.populateInterfaceFromJson(
+        systemConfiguration, jsonPointerPath, iface, dict);
+
+    auto handler = [](const DBusInterface& value) {
+        EXPECT_EQ(value.size(), 3);
+
+        auto vectorStrings = std::get<std::vector<std::string>>(
+            std::get<1>(*value.find("ArrayOfStrings")));
+        EXPECT_EQ(vectorStrings.size(), 3);
+        EXPECT_EQ(vectorStrings[1], "string2");
+    };
+    postAssertHandler(handler, objPath, configInterface);
+}
