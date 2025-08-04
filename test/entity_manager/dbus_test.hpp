3cb164169598201f53b1afffd62c87256620fedd
Alexander Hansen <alexander.hansen@9elements.com>
2025-08-04 16:27:10 +0200

    test: entity-manager: dbus interface
    Write tests for the EM DBus interfaces.
    
    Given a piece of json configuration record, call the appropriate
    functions to expose it on DBus.
    
    Then make dbus calls to query the resulting DBus objects.
    
    Tested: Unit Tests Pass.
    
    Change-Id: I67acef6ecde05c18e99221c4a148fa565e9d9463
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ test/entity_manager/dbus_test.hpp
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "test_em.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+using namespace std::string_literals;
+
+using DBusPropertiesMap = boost::container::flat_map<
+    std::string, std::variant<std::string, int64_t, double, bool,
+                              std::vector<int64_t>, std::vector<uint64_t>,
+                              std::vector<double>, std::vector<std::string>>>;
+
+class DBusTest : public testing::Test
+{
+  protected:
+    DBusTest() :
+        systemBus(std::make_shared<sdbusplus::asio::connection>(io)),
+        em(systemBus, io)
+    {}
+    ~DBusTest() noexcept override = default;
+
+    boost::asio::io_context io;
+    std::shared_ptr<sdbusplus::asio::connection> systemBus;
+    TestEM em;
+    nlohmann::json systemConfiguration = "{}";
+    std::string boardNameOrig = "myboard";
+    std::string jsonPointerPath = "xyz";
+
+    std::string configInterface = "xyz.openbmc_project.Configuration.Example";
+    std::string objPath =
+        "/xyz/openbmc_project/inventory/system/board/MyBoard/MyRecord";
+
+    // This is written in the testcase
+    std::shared_ptr<sdbusplus::asio::dbus_interface> iface;
+
+    // The configuration to expose. Gets written in the testcase
+    nlohmann::json dict;
+
+  public:
+    DBusTest(const DBusTest&) = delete;
+    DBusTest(DBusTest&&) = delete;
+    DBusTest& operator=(const DBusTest&) = delete;
+    DBusTest& operator=(DBusTest&&) = delete;
+
+    void postAssertHandler(
+        const std::function<void(const DBusPropertiesMap& value)>& handler,
+        std::string& path, std::string& interface, bool stopOnError = true)
+    {
+        auto h3 = [this, handler, stopOnError](boost::system::error_code ec,
+                                               const DBusPropertiesMap& value) {
+            EXPECT_FALSE(ec);
+            if (ec)
+            {
+                lg2::error("{ERROR}", "ERROR", ec.message());
+
+                // not run below line to manually inspect DBus in case of test
+                // failure
+                if (stopOnError)
+                {
+                    io.stop();
+                }
+
+                return;
+            }
+
+            try
+            {
+                handler(value);
+                io.stop();
+            }
+            catch (std::exception& e)
+            {
+                if (stopOnError)
+                {
+                    io.stop();
+                }
+            }
+        };
+
+        boost::asio::post(io, [this, path, interface, h3]() {
+            systemBus->async_method_call(h3, em.busName, path,
+                                         "org.freedesktop.DBus.Properties",
+                                         "GetAll", interface);
+        });
+
+        io.run();
+    };
+};
