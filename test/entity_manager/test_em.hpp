368803c6d31ee121cf25bd2c16ed3ecf5c07f3ea
Alexander Hansen <alexander.hansen@9elements.com>
2025-08-04 16:27:10 +0200

    test: entity-manager: dbus interface
    Write tests for the EM DBus interfaces.
    
    Given a piece of json configuration record, call the appropriate
    functions to expose it on DBus.
    
    Then make dbus calls to query the resulting DBus objects.
    
    Tested: Unit Tests Pass.
    
    Change-Id: I67acef6ecde05c18e99221c4a148fa565e9d9463
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- test/entity_manager/test_em.hpp
+++ test/entity_manager/test_em.hpp
@@ -30,10 +30,12 @@ class TestEM : public EntityManager
     TestEM(std::shared_ptr<sdbusplus::asio::connection>& systemBus,
            boost::asio::io_context& io) : EntityManager(systemBus, io, false)
     {
-        std::string busName = getRandomBusName();
+        busName = getRandomBusName();
         systemBus->request_name(busName.c_str());
         lg2::debug("requesting bus name {NAME}", "NAME", busName);
     };
+
+    std::string busName;
 
     // declare protected members of the base class as public here
     using EntityManager::dbusMatches;
