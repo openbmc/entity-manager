6c356846e61598198f49ef9250c12621564e1acb
Alexander Hansen <alexander.hansen@9elements.com>
2025-08-20 14:43:04 +0200

    topology: refactor upstreamPorts, downstreamPorts
    go from
    ```
    std::unordered_map<PortType, std::vector<Path>> upstreamPorts;
    std::unordered_map<PortType, std::vector<Path>> downstreamPorts;
    ```
    to
    ```
    std::unordered_map<PortType, std::set<Path>> upstreamPorts;
    std::unordered_map<PortType, std::set<Path>> downstreamPorts;
    ```
    
    since a given inventory item can only be an upstream once for a given
    association.
    
    Also, associations do not have any 'order' in which they need to be
    created so we do not need any specific ordering as with a vector.
    
    Tested: Topology Unit Tests Pass
    
    Change-Id: I47e49343992eaad7ea2587e71a5499bc3dccc064
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- src/entity_manager/topology.hpp
+++ src/entity_manager/topology.hpp
@@ -30,8 +30,8 @@ class Topology
     void addUpstreamPort(const Path& path, const BoardType& boardType,
                          const PortType& exposesType);
 
-    std::unordered_map<PortType, std::vector<Path>> upstreamPorts;
-    std::unordered_map<PortType, std::vector<Path>> downstreamPorts;
+    std::unordered_map<PortType, std::set<Path>> upstreamPorts;
+    std::unordered_map<PortType, std::set<Path>> downstreamPorts;
     std::set<Path> powerPaths;
     std::unordered_map<Path, BoardType> boardTypes;
     std::unordered_map<BoardName, Path> boardNames;
