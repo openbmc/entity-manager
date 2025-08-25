ecf1a31636a65f3eb2aaa7e45c8b51b900bedf7b
George Liu <liuxiwei@ieisystem.com>
2025-08-25 10:43:12 +0800

    Remove boost::split
    Replaced boost::split with a simple std::string_view-based split to
    reduce Boost dependency and template instantiation during compilation
    
    Tested: added UT and verified all tests passed
    
    Change-Id: Icc84794a3d5a98088bdbce032dc76055a035f0dc
    Signed-off-by: George Liu <liuxiwei@ieisystem.com>

--- src/utils.hpp
+++ src/utils.hpp
@@ -95,6 +95,8 @@ auto iFindFirst(T&& str, std::string_view sub)
     });
 }
 
+std::vector<std::string> split(std::string_view str, char delim);
+
 template <typename T>
 std::from_chars_result fromCharsWrapper(const std::string_view& str, T& out,
                                         bool& fullMatch, int base = 10)
