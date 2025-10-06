6c96e0b843d7ec6891c99e8241e059014fcf92ae
Alexander Hansen <alexander.hansen@9elements.com>
2025-10-06 19:03:11 +0200

    entity-manager: multi-host Power Status Monitor
    Currently power status monitor is hardcoded to use host index 0 which
    assumes a single host system.
    
    Patch has 2 objectives
    
    - Allow multi-host systems to correctly use `PowerState` property in EM
      configs, getting Host Index from Managed Host interface
    
    - Move Power Status Monitor away from hardcoding the object path.
    
    Couple of gotchas still remain, the devices connected to a managed host
    interface (in terms of association) should also receive the correct
    power state. Which is currently not the case as of this patch, and may
    need to be fixed in the future.
    
    Tested: on Tyan S8030
    
    Starting EM with patch applied:
    
    ```
    Nov 18 16:42:20 s8030-bmc-30303035c0c1 systemd[1]: Started Entity Manager.
    Nov 18 16:42:29 s8030-bmc-30303035c0c1 entity-manager[3047]: Finished loading json configuration in 8689ms
    Nov 18 16:42:29 s8030-bmc-30303035c0c1 entity-manager[3047]: found host 0 power state on path /xyz/openbmc_project/state/host0
    Nov 18 16:42:29 s8030-bmc-30303035c0c1 entity-manager[3047]: setting up power state change match for host 0
    Nov 18 16:42:29 s8030-bmc-30303035c0c1 entity-manager[3047]: querying initial power state
    ```
    
    then power on the host via `ipmitool power on`.
    
    ```
    
    Nov 18 16:53:09 s8030-bmc-30303035c0c1 entity-manager[3047]: power match triggered on /xyz/openbmc_project/state/host0
    Nov 18 16:53:09 s8030-bmc-30303035c0c1 entity-manager[3047]: power match triggered on /xyz/openbmc_project/state/host0
    Nov 18 16:53:09 s8030-bmc-30303035c0c1 entity-manager[3047]: Host 0 power status: on
    Nov 18 16:53:09 s8030-bmc-30303035c0c1 entity-manager[3047]: power match triggered on /xyz/openbmc_project/state/host0
    Nov 18 16:53:09 s8030-bmc-30303035c0c1 entity-manager[3047]: Host 0 power status: off
    
    Nov 18 16:53:10 s8030-bmc-30303035c0c1 entity-manager[3047]: power match triggered on /xyz/openbmc_project/state/host0
    Nov 18 16:53:10 s8030-bmc-30303035c0c1 entity-manager[3047]: Host 0 power status: on
    ```
    
    powering off again:
    
    ```
    Nov 18 16:53:44 s8030-bmc-30303035c0c1 entity-manager[3047]: power match triggered on /xyz/openbmc_project/state/host0
    Nov 18 16:53:44 s8030-bmc-30303035c0c1 entity-manager[3047]: Host 0 power status: off
    ```
    
    Change-Id: Id70a547e489e1e552f83c576875f42f5dbe09f10
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ src/entity_manager/managed_host.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <nlohmann/json.hpp>
+
+#include <string>
+
+namespace managed_host
+{
+
+std::string hostPowerPath(size_t hostIndex);
+
+// @returns < 0 on error
+ssize_t hostIndexFromPath(const std::string& path);
+
+// @param name     device name used for error logging
+// @param config   reference to a single EM config
+size_t getManagedHostIndex(const std::string& name,
+                           const nlohmann::json::object_t* config);
+
+} // namespace managed_host
--- src/entity_manager/power_status_monitor.cpp
+++ src/entity_manager/power_status_monitor.cpp
@@ -1,9 +1,11 @@
 #include "power_status_monitor.hpp"
 
+#include "managed_host.hpp"
 #include "utils.hpp"
 
 #include <phosphor-logging/lg2.hpp>
 #include <sdbusplus/bus/match.hpp>
+#include <xyz/openbmc_project/ObjectMapper/client.hpp>
 #include <xyz/openbmc_project/State/Host/client.hpp>
 
 #include <flat_map>
@@ -12,61 +14,131 @@ namespace power
 {
 
 using HostState = sdbusplus::common::xyz::openbmc_project::state::Host;
+using ObjectMapper = sdbusplus::common::xyz::openbmc_project::ObjectMapper;
 
-const static std::string path =
-    std::format("{}/{}{}", HostState::namespace_path::value,
-                HostState::namespace_path::host, 0);
-const static constexpr char* property = "CurrentHostState";
+const std::string powerBasePath = std::format(
+    "{}/{}", HostState::namespace_path::value, HostState::namespace_path::host);
 
 PowerStatusMonitor::PowerStatusMonitor(sdbusplus::asio::connection& conn) :
-
-    powerMatch(static_cast<sdbusplus::bus_t&>(conn),
-               "type='signal',interface='" +
-                   std::string(em_utils::properties::interface) + "',path='" +
-                   std::string(power::path) + "',arg0='" +
-                   std::string(HostState::interface) + "'",
-               std::bind_front(&PowerStatusMonitor::handlePowerMatch, this))
-
+    conn(conn)
 {
-    getInitialPowerStatus(conn);
+    asyncGetPowerPaths(
+        std::bind_front(&PowerStatusMonitor::afterGetPowerPaths, this));
 }
 
-bool PowerStatusMonitor::isPowerOn() const
+bool PowerStatusMonitor::isPowerOn(size_t hostIndex) const
 {
-    return powerStatusOn;
+    return powerStatusOn.at(hostIndex);
 }
 
 void PowerStatusMonitor::handlePowerMatch(sdbusplus::message_t& message)
 {
-    lg2::debug("power match triggered");
+    const std::string path(message.get_path());
+
+    lg2::debug("power match triggered on {PATH}", "PATH", path);
 
     std::string objectName;
     std::flat_map<std::string, std::variant<std::string>> values;
     message.read(objectName, values);
-    auto findState = values.find(power::property);
+    auto findState = values.find(HostState::property_names::current_host_state);
+
+    ssize_t hostIndex = managed_host::hostIndexFromPath(path);
+
+    if (hostIndex < 0)
+    {
+        lg2::error(
+            "Could not find host index for {PATH}, cannot update host power status",
+            "PATH", path);
+
+        return;
+    }
+
     if (findState != values.end())
     {
-        powerStatusOn =
+        powerStatusOn[hostIndex] =
             std::get<std::string>(findState->second).ends_with("Running");
+
+        lg2::debug("Host {HOSTINDEX} power status: {STATUS}", "HOSTINDEX",
+                   hostIndex, "STATUS",
+                   (powerStatusOn[hostIndex] ? "on" : "off"));
     }
 }
 
-void PowerStatusMonitor::getInitialPowerStatus(
-    sdbusplus::asio::connection& conn)
+void PowerStatusMonitor::getInitialPowerStatus(size_t hostIndex)
 {
     lg2::debug("querying initial power state");
 
+    const std::string powerPath = managed_host::hostPowerPath(hostIndex);
+
     conn.async_method_call(
-        [this](boost::system::error_code ec,
-               const std::variant<std::string>& state) {
+        [this, hostIndex,
+         powerPath{powerPath}](boost::system::error_code ec,
+                               const std::variant<std::string>& state) {
             if (ec)
             {
                 return;
             }
-            powerStatusOn = std::get<std::string>(state).ends_with("Running");
+            powerStatusOn[hostIndex] =
+                std::get<std::string>(state).ends_with("Running");
         },
-        HostState::interface, power::path, em_utils::properties::interface,
-        em_utils::properties::get, HostState::interface, power::property);
+        HostState::interface, powerPath, em_utils::properties::interface,
+        em_utils::properties::get, HostState::interface,
+        HostState::property_names::current_host_state);
+}
+
+void PowerStatusMonitor::asyncGetPowerPaths(
+    const std::function<void(boost::system::error_code ec,
+                             const GetSubTreeType& subtree)>& callback)
+{
+    std::vector<std::string> powerInterfaces = {HostState::interface};
+    conn.async_method_call(callback, ObjectMapper::default_service,
+                           ObjectMapper::instance_path, ObjectMapper::interface,
+                           ObjectMapper::method_names::get_sub_tree, "/", 0,
+                           std::move(powerInterfaces));
+}
+
+void PowerStatusMonitor::setupPowerMatch(size_t hostIndex)
+{
+    lg2::debug("setting up power state change match for host {HOSTINDEX}",
+               "HOSTINDEX", hostIndex);
+
+    const std::string powerPath = managed_host::hostPowerPath(hostIndex);
+
+    powerMatches.emplace(
+        hostIndex,
+        sdbusplus::bus::match_t(
+            static_cast<sdbusplus::bus_t&>(conn),
+            "type='signal',interface='" +
+                std::string(em_utils::properties::interface) + "',path='" +
+                powerPath + "',arg0='" + std::string(HostState::interface) +
+                "'",
+            std::bind_front(&PowerStatusMonitor::handlePowerMatch, this)));
+}
+
+void PowerStatusMonitor::afterGetPowerPaths(boost::system::error_code ec,
+                                            const GetSubTreeType& subtree)
+{
+    if (ec.value() == ENOENT)
+    {
+        return; // wasn't found by mapper
+    }
+
+    for (const auto& [path, object] : subtree)
+    {
+        const ssize_t hostIndex = managed_host::hostIndexFromPath(path);
+
+        if (hostIndex < 0)
+        {
+            continue;
+        }
+
+        lg2::debug("found host {HOSTINDEX} power state on path {PATH}",
+                   "HOSTINDEX", hostIndex, "PATH", path);
+
+        powerStatusOn[hostIndex] = false;
+        setupPowerMatch(hostIndex);
+        getInitialPowerStatus(hostIndex);
+    }
 }
 
 } // namespace power
