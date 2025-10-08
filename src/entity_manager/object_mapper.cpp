a614db21eb0c66ec007a46892cce029951a1ae4f
Alexander Hansen <alexander.hansen@9elements.com>
2025-10-08 16:30:58 +0200

    entity-manager: extract getSubTree function
    Extract this function to separate ObjectMapper usage from the rest of
    the code. Mapper specifics such as object path and service name do not
    have to be hardcoded and code is made reusable.
    
    Additional debug capability is provided via lg2::debug call for
    ObjectMapper usage.
    
    Tested: on Tyan S8030
    
    ```
    Oct 08 15:17:01 s8030-bmc-30303035c0c1 systemd[1]: Started Entity Manager.
    Oct 08 15:17:10 s8030-bmc-30303035c0c1 entity-manager[24399]: Finished loading json configuration in 8728ms
    Oct 08 15:17:11 s8030-bmc-30303035c0c1 entity-manager[24399]: GetSubTree path=/ depth=0 of 9 interfaces
    Oct 08 15:17:11 s8030-bmc-30303035c0c1 entity-manager[24399]: creating PropertiesChanged match on /xyz/openbmc_project/FruDevice/ConnectX_6_Dx_100GE_2P_NIC
    Oct 08 15:17:11 s8030-bmc-30303035c0c1 entity-manager[24399]: creating PropertiesChanged match on /xyz/openbmc_project/FruDevice/PWS_920P_SQ
    Oct 08 15:17:11 s8030-bmc-30303035c0c1 entity-manager[24399]: creating PropertiesChanged match on /xyz/openbmc_project/FruDevice/PWS_920P_SQ_0
    ```
    
    Later it finds probe matches
    ```
    Oct 08 15:17:11 s8030-bmc-30303035c0c1 entity-manager[24399]: Found probe match on /xyz/openbmc_project/FruDevice/PWS_920P_SQ xyz.openbmc_project.FruDevice
    Oct 08 15:17:11 s8030-bmc-30303035c0c1 entity-manager[24399]: Found probe match on /xyz/openbmc_project/FruDevice/PWS_920P_SQ_0 xyz.openbmc_project.FruDevice
    ```
    
    and further GetSubTree calls
    ```
    Oct 08 15:17:12 s8030-bmc-30303035c0c1 entity-manager[24399]: GetSubTree path=/ depth=0 of 8 interfaces
    ...
    Oct 08 15:17:17 s8030-bmc-30303035c0c1 entity-manager[24399]: GetSubTree path=/ depth=0 of 9 interfaces
    ...
    Oct 08 15:17:20 s8030-bmc-30303035c0c1 entity-manager[24399]: GetSubTree path=/ depth=0 of 7 interfaces
    ```
    
    host power state change via `ipmitool power on` triggers further
    GetSubTree calls.
    
    ```
    Oct 08 15:23:08 s8030-bmc-30303035c0c1 entity-manager[24399]: power match triggered
    Oct 08 15:23:08 s8030-bmc-30303035c0c1 entity-manager[24399]: power match triggered
    Oct 08 15:23:08 s8030-bmc-30303035c0c1 entity-manager[24399]: power match triggered
    Oct 08 15:23:14 s8030-bmc-30303035c0c1 entity-manager[24399]: GetSubTree path=/ depth=0 of 9 interfaces
    ...
    ```
    
    `busctl tree` output is as before.
    
    Change-Id: Id93ad967abfc1a5f512ec9331d2bf56ca7e921d2
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ src/entity_manager/object_mapper.cpp
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+
+#include "object_mapper.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <xyz/openbmc_project/ObjectMapper/common.hpp>
+
+#include <flat_set>
+
+void object_mapper::getSubTree(
+    sdbusplus::asio::connection& systemBus, const std::string& path,
+    const int32_t depth,
+    const std::flat_set<std::string, std::less<>>& interfaces,
+    std::move_only_function<void(boost::system::error_code& ec,
+                                 const GetSubTreeType& interfaceSubtree)>&&
+        callback)
+{
+    using ObjectMapper = sdbusplus::common::xyz::openbmc_project::ObjectMapper;
+
+    lg2::debug("GetSubTree path={PATH} depth={DEPTH} of {N} interfaces", "PATH",
+               path, "DEPTH", depth, "N", interfaces.size());
+
+    systemBus.async_method_call(
+        std::move(callback), ObjectMapper::default_service,
+        ObjectMapper::instance_path, ObjectMapper::interface,
+        ObjectMapper::method_names::get_sub_tree, path, depth, interfaces);
+}
---
+++ src/entity_manager/object_mapper.hpp
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+
+#pragma once
+
+#include <sdbusplus/asio/connection.hpp>
+
+#include <flat_set>
+
+using GetSubTreeType = std::vector<
+    std::pair<std::string,
+              std::vector<std::pair<std::string, std::vector<std::string>>>>>;
+
+namespace object_mapper
+{
+
+void getSubTree(
+    sdbusplus::asio::connection& systemBus, const std::string& path,
+    int32_t depth, const std::flat_set<std::string, std::less<>>& interfaces,
+    std::move_only_function<void(boost::system::error_code& ec,
+                                 const GetSubTreeType& interfaceSubtree)>&&
+        callback);
+
+};
