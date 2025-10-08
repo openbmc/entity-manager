02fb0ddc9ceb7a75667de3936e6af855508de4de
Alexander Hansen <alexander.hansen@9elements.com>
2025-10-08 16:30:58 +0200

    entity-manager: extract getSubTree function
    Extract this function to separate ObjectMapper usage from the rest of
    the code. Mapper specifics such as object path and service name do not
    have to be hardcoded and code is made reusable.
    
    Additional debug capability is provided via lg2::debug call for
    ObjectMapper usage.
    
    Also change the `boost::container::flat_set` to `std::set` to align with
    recent patches, preferring STL containers over boost where possible.
    
    On my system the container is at most holding 9 interfaces at a time,
    performance of the container should not be a concern.
    
    Tested: on Tyan S8030
    
    ```
    Oct 08 15:17:01 s8030-bmc-30303035c0c1 systemd[1]: Started Entity Manager.
    Oct 08 15:17:10 s8030-bmc-30303035c0c1 entity-manager[24399]: Finished loading json configuration in 8728ms
    Oct 08 15:17:11 s8030-bmc-30303035c0c1 entity-manager[24399]: GetSubTree path=/ depth=0 of 9 interfaces
    Oct 08 15:17:11 s8030-bmc-30303035c0c1 entity-manager[24399]: creating PropertiesChanged match on /xyz/openbmc_project/FruDevice/ConnectX_6_Dx_100GE_2P_NIC
    Oct 08 15:17:11 s8030-bmc-30303035c0c1 entity-manager[24399]: creating PropertiesChanged match on /xyz/openbmc_project/FruDevice/PWS_920P_SQ
    Oct 08 15:17:11 s8030-bmc-30303035c0c1 entity-manager[24399]: creating PropertiesChanged match on /xyz/openbmc_project/FruDevice/PWS_920P_SQ_0
    ```
    
    Later it finds probe matches
    ```
    Oct 08 15:17:11 s8030-bmc-30303035c0c1 entity-manager[24399]: Found probe match on /xyz/openbmc_project/FruDevice/PWS_920P_SQ xyz.openbmc_project.FruDevice
    Oct 08 15:17:11 s8030-bmc-30303035c0c1 entity-manager[24399]: Found probe match on /xyz/openbmc_project/FruDevice/PWS_920P_SQ_0 xyz.openbmc_project.FruDevice
    ```
    
    and further GetSubTree calls
    ```
    Oct 08 15:17:12 s8030-bmc-30303035c0c1 entity-manager[24399]: GetSubTree path=/ depth=0 of 8 interfaces
    ...
    Oct 08 15:17:17 s8030-bmc-30303035c0c1 entity-manager[24399]: GetSubTree path=/ depth=0 of 9 interfaces
    ...
    Oct 08 15:17:20 s8030-bmc-30303035c0c1 entity-manager[24399]: GetSubTree path=/ depth=0 of 7 interfaces
    ```
    
    host power state change via `ipmitool power on` triggers further
    GetSubTree calls.
    
    ```
    Oct 08 15:23:08 s8030-bmc-30303035c0c1 entity-manager[24399]: power match triggered
    Oct 08 15:23:08 s8030-bmc-30303035c0c1 entity-manager[24399]: power match triggered
    Oct 08 15:23:08 s8030-bmc-30303035c0c1 entity-manager[24399]: power match triggered
    Oct 08 15:23:14 s8030-bmc-30303035c0c1 entity-manager[24399]: GetSubTree path=/ depth=0 of 9 interfaces
    ...
    ```
    
    `busctl tree` output is as before.
    
    Change-Id: Id93ad967abfc1a5f512ec9331d2bf56ca7e921d2
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ src/entity_manager/object_mapper.hpp
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: Copyright OpenBMC Authors
+
+#pragma once
+
+#include <sdbusplus/asio/connection.hpp>
+
+#include <set>
+
+using GetSubTreeType = std::vector<
+    std::pair<std::string,
+              std::vector<std::pair<std::string, std::vector<std::string>>>>>;
+
+namespace object_mapper
+{
+
+void getSubTree(
+    sdbusplus::asio::connection& systemBus, const std::string& path,
+    int32_t depth, const std::set<std::string>& interfaces,
+    std::function<void(boost::system::error_code& ec,
+                       const GetSubTreeType& interfaceSubtree)>& callback);
+
+};
--- src/entity_manager/perform_scan.cpp
+++ src/entity_manager/perform_scan.cpp
@@ -3,6 +3,7 @@
 
 #include "perform_scan.hpp"
 
+#include "object_mapper.hpp"
 #include "perform_probe.hpp"
 #include "utils.hpp"
 
@@ -12,12 +13,6 @@
 #include <phosphor-logging/lg2.hpp>
 
 #include <charconv>
-
-using GetSubTreeType = std::vector<
-    std::pair<std::string,
-              std::vector<std::pair<std::string, std::vector<std::string>>>>>;
-
-constexpr const int32_t maxMapperDepth = 0;
 
 struct DBusInterfaceInstance
 {
@@ -93,13 +88,49 @@ static void processDbusObjects(
     }
 }
 
+void scan::afterFindDBusObjects(
+    boost::asio::io_context& io, std::set<std::string> interfaces,
+    std::vector<std::shared_ptr<probe::PerformProbe>> probeVector,
+    const std::shared_ptr<scan::PerformScan>& scan, size_t retries,
+    boost::system::error_code ec, const GetSubTreeType& interfaceSubtree)
+{
+    if (ec)
+    {
+        if (ec.value() == ENOENT)
+        {
+            return; // wasn't found by mapper
+        }
+        lg2::error("Error communicating to mapper");
+
+        if (retries == 0U)
+        {
+            // if we can't communicate to the mapper something is very
+            // wrong
+            std::exit(EXIT_FAILURE);
+        }
+
+        auto timer = std::make_shared<boost::asio::steady_timer>(io);
+        timer->expires_after(std::chrono::seconds(10));
+
+        timer->async_wait([timer, interfaces{std::move(interfaces)}, scan,
+                           probeVector{std::move(probeVector)}, retries,
+                           &io](const boost::system::error_code&) mutable {
+            scan::findDbusObjects(std::move(probeVector), std::move(interfaces),
+                                  scan, io, retries - 1);
+        });
+        return;
+    }
+
+    processDbusObjects(probeVector, scan, interfaceSubtree, io);
+}
+
 // Populates scan->dbusProbeObjects with all interfaces and properties
 // for the paths that own the interfaces passed in.
-void findDbusObjects(
-    std::vector<std::shared_ptr<probe::PerformProbe>>&& probeVector,
-    boost::container::flat_set<std::string>&& interfaces,
+void scan::findDbusObjects(
+    std::vector<std::shared_ptr<probe::PerformProbe>> probeVector,
+    std::set<std::string> interfaces,
     const std::shared_ptr<scan::PerformScan>& scan, boost::asio::io_context& io,
-    size_t retries = 5)
+    size_t retries)
 {
     // Filter out interfaces already obtained.
     for (const auto& [path, probeInterfaces] : scan->dbusProbeObjects)
@@ -114,46 +145,17 @@ void findDbusObjects(
         return;
     }
 
+    std::function<void(boost::system::error_code&,
+                       const GetSubTreeType& interfaceSubtree)>
+        cb = [probeVector{std::move(probeVector)}, scan, retries, &io,
+              interfaces](boost::system::error_code& ec,
+                          const GetSubTreeType& interfaceSubtree) mutable {
+            scan::afterFindDBusObjects(io, interfaces, probeVector, scan,
+                                       retries, ec, interfaceSubtree);
+        };
+
     // find all connections in the mapper that expose a specific type
-    scan->_em.systemBus->async_method_call(
-        [interfaces, probeVector{std::move(probeVector)}, scan, retries,
-         &io](boost::system::error_code& ec,
-              const GetSubTreeType& interfaceSubtree) mutable {
-            if (ec)
-            {
-                if (ec.value() == ENOENT)
-                {
-                    return; // wasn't found by mapper
-                }
-                lg2::error("Error communicating to mapper.");
-
-                if (retries == 0U)
-                {
-                    // if we can't communicate to the mapper something is very
-                    // wrong
-                    std::exit(EXIT_FAILURE);
-                }
-
-                auto timer = std::make_shared<boost::asio::steady_timer>(io);
-                timer->expires_after(std::chrono::seconds(10));
-
-                timer->async_wait(
-                    [timer, interfaces{std::move(interfaces)}, scan,
-                     probeVector{std::move(probeVector)}, retries,
-                     &io](const boost::system::error_code&) mutable {
-                        findDbusObjects(std::move(probeVector),
-                                        std::move(interfaces), scan, io,
-                                        retries - 1);
-                    });
-                return;
-            }
-
-            processDbusObjects(probeVector, scan, interfaceSubtree, io);
-        },
-        "xyz.openbmc_project.ObjectMapper",
-        "/xyz/openbmc_project/object_mapper",
-        "xyz.openbmc_project.ObjectMapper", "GetSubTree", "/", maxMapperDepth,
-        interfaces);
+    object_mapper::getSubTree(*scan->_em.systemBus, "/", 0, interfaces, cb);
 }
 
 static std::string getRecordName(const DBusInterface& probe,
@@ -529,7 +531,7 @@ void scan::PerformScan::updateSystemConfiguration(
 
 void scan::PerformScan::run()
 {
-    boost::container::flat_set<std::string> dbusProbeInterfaces;
+    std::set<std::string> dbusProbeInterfaces;
     std::vector<std::shared_ptr<probe::PerformProbe>> dbusProbePointers;
 
     for (auto it = _configurations.begin(); it != _configurations.end();)
