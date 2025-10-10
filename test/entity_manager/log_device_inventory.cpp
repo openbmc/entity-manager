864995e1de54403c946cbc4bd60efa4a8fc88249
Alexander Hansen <alexander.hansen@9elements.com>
2025-10-10 22:58:01 +0200

    entity-manager: fixes for logDeviceAdded/Removed
    Follow-up patch to fix issues found in comments of [1].
    
    - Use explicit type rather than 'auto' for local variable
    - Setup a return variable to avoid duplicate initial value "Unknown"
    - Fix string value read to avoid uncaught exception
    - Remove unused boost include
    - Add unit tests
    
    Tested: Unit tests pass.
    
    References:
    [1] https://gerrit.openbmc.org/c/openbmc/entity-manager/+/84340
    
    Change-Id: I3d5540860e8ef8e590bc2685ce559c53dc8452b5
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ test/entity_manager/log_device_inventory.cpp
@@ -0,0 +1,49 @@
+
+#include "entity_manager/log_device_inventory.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(LogDevicInventory, QueryInvInfoSuccess)
+{
+    nlohmann::json record = nlohmann::json::parse(R"(
+{
+    "Name": "Supermicro PWS 920P SQ 0",
+    "Type": "PowerSupply",
+    "xyz.openbmc_project.Inventory.Decorator.Asset": {
+        "Manufacturer": "Supermicro",
+        "Model": "PWS 920P SQ",
+        "PartNumber": "328923",
+        "SerialNumber": "43829239"
+    }
+}
+   )");
+
+    InvAddRemoveInfo info = queryInvInfo(record);
+
+    EXPECT_EQ(info.name, "Supermicro PWS 920P SQ 0");
+    EXPECT_EQ(info.type, "PowerSupply");
+    EXPECT_EQ(info.sn, "43829239");
+    EXPECT_EQ(info.model, "PWS 920P SQ");
+}
+
+TEST(LogDevicInventory, QueryInvInfoNoModelFound)
+{
+    nlohmann::json record = nlohmann::json::parse(R"(
+{
+    "Name": "Supermicro PWS 920P SQ 0",
+    "Type": "PowerSupply",
+    "xyz.openbmc_project.Inventory.Decorator.Asset": {
+        "Manufacturer": "Supermicro",
+        "PartNumber": "328923",
+        "SerialNumber": "43829239"
+    }
+}
+    )");
+
+    InvAddRemoveInfo info = queryInvInfo(record);
+
+    EXPECT_EQ(info.name, "Supermicro PWS 920P SQ 0");
+    EXPECT_EQ(info.type, "PowerSupply");
+    EXPECT_EQ(info.sn, "43829239");
+    EXPECT_EQ(info.model, "Unknown");
+}
