88052aec4cda70b3cc35c200e62769f5610461e8
Alexander Hansen <alexander.hansen@9elements.com>
2025-11-13 12:29:03 +0100

    entity-manager: not mark destructors as virtual
    We are not deriving from PerformProbe or PerformScan.
    
    Mark these classes as `final` to be clear.
    
    Tested: on Tyan S8030
    
    EM (re)starts normally, no errors observed. All devices present on D-Bus
    as expected.
    
    Change-Id: I50a09cca9323e284a0ba7b93d994f3a20fc2f558
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- src/entity_manager/perform_probe.hpp
+++ src/entity_manager/perform_probe.hpp
@@ -26,13 +26,13 @@ using FoundProbeTypeT = std::optional<probe_type_codes>;
 FoundProbeTypeT findProbeType(const std::string& probe);
 
 // this class finds the needed dbus fields and on destruction runs the probe
-struct PerformProbe
+struct PerformProbe final
 {
     PerformProbe(nlohmann::json& recordRef,
                  const std::vector<std::string>& probeCommand,
                  std::string probeName,
                  std::shared_ptr<scan::PerformScan>& scanPtr);
-    virtual ~PerformProbe();
+    ~PerformProbe();
 
   private:
     nlohmann::json& recordRef;
