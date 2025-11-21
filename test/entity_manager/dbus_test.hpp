2e37fb5717b2bda562198d4088f4edd668df2366
Alexander Hansen <alexander.hansen@9elements.com>
2025-11-21 10:21:05 +0100

    test: entity-manger: end-to-end probe true
    This patch adds an end-to-end integration test for entity-manager.
    An example configuration is written to a configuration directory, EM
    picks that up exposes the configuration due to Probe: "TRUE" statement.
    
    After EM initializes, an assertion handler is run to confirm that
    the expected configuration interface has been created on the expected
    DBus path.
    
    The testcaes represents more coverage than the preceding test for DBus
    interfaces since loading and processing of configuration is also tested.
    
    Possible follow-up works:
    
    - end-to-end testcase which does not use Probe: TRUE but an actual DBus
      interface
    
    Tested: Unit Test Pass
    
    Change-Id: I9757997b34026f3b71c83ad2f5c17e2253370a2d
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- test/entity_manager/dbus_test.hpp
+++ test/entity_manager/dbus_test.hpp
@@ -2,6 +2,7 @@
 
 #include "test_em.hpp"
 
+#include <boost/asio/steady_timer.hpp>
 #include <nlohmann/json.hpp>
 
 #include <string>
@@ -50,10 +51,27 @@ class DBusTest : public testing::Test
 
     void postAssertHandler(
         const std::function<void(const DBusPropertiesMap& value)>& handler,
-        std::string& path, std::string& interface, bool stopOnError = true)
+        const std::string& busName, const sdbusplus::message::object_path& path,
+        const std::string& interface, bool stopOnError = true,
+        const size_t timeoutMs = 1)
     {
-        auto h3 = [this, handler, stopOnError](boost::system::error_code ec,
-                                               const DBusPropertiesMap& value) {
+        staticPostAssertHandler(io, systemBus, handler, busName, path,
+                                interface, timeoutMs, stopOnError);
+    }
+
+    static void staticPostAssertHandler(
+        boost::asio::io_context& io,
+        const std::shared_ptr<sdbusplus::asio::connection>& systemBus,
+        const std::function<void(const DBusPropertiesMap& value)>& handler,
+        const std::string& busName, const sdbusplus::message::object_path& path,
+        const std::string& interface, const size_t timeoutMs,
+        bool stopOnError = true)
+    {
+        lg2::debug("test: running assertion handler or {BUS} {PATH} {INTF}",
+                   "BUS", busName, "PATH", path, "INTF", interface);
+
+        auto h3 = [&io, handler, stopOnError](boost::system::error_code ec,
+                                              const DBusPropertiesMap& value) {
             EXPECT_FALSE(ec);
             if (ec)
             {
@@ -71,11 +89,13 @@ class DBusTest : public testing::Test
 
             try
             {
+                lg2::debug("test: running assertion handler");
                 handler(value);
                 io.stop();
             }
             catch (std::exception& e)
             {
+                lg2::error("caught exception: {ERR}", "ERR", e);
                 if (stopOnError)
                 {
                     io.stop();
@@ -83,10 +103,26 @@ class DBusTest : public testing::Test
             }
         };
 
-        boost::asio::post(io, [this, path, interface, h3]() {
-            systemBus->async_method_call(h3, em.busName, path,
-                                         "org.freedesktop.DBus.Properties",
-                                         "GetAll", interface);
+        boost::asio::steady_timer timer(io);
+        // we have to wait until properties changed is done
+
+        lg2::debug("setting timer to {VALUE} ms", "VALUE", timeoutMs);
+        timer.expires_after(std::chrono::milliseconds(timeoutMs));
+
+        boost::asio::post(io, [systemBus, busName, path, interface, &h3,
+                               &timer]() {
+            timer.async_wait([&h3, busName, path, interface,
+                              systemBus](const boost::system::error_code& ec) {
+                lg2::debug("timer expired");
+                if (ec)
+                {
+                    lg2::error("timer error");
+                    return;
+                }
+                systemBus->async_method_call(h3, busName, path,
+                                             "org.freedesktop.DBus.Properties",
+                                             "GetAll", interface);
+            });
         });
 
         io.run();
