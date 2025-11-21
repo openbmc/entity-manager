b8d64f1eaf65a12f6c2dd9e27bf6774170d9c539
Alexander Hansen <alexander.hansen@9elements.com>
2025-11-21 14:07:45 +0100

    entity-manager: propertiesChangedCallback delay
    Parameterize the arbitrary delay for properties changed callback.
    
    Routing it out as a parameter helps tests to run faster while still
    keeping the same control flow as before.
    
    Tested: on Tyan S8030
    
    propertiesChangedCalback delay is as expected and EM successfully probes
    configuration.
    
    ```
    ...
    Nov 25 13:40:16 s8030-bmc-30303035c0c1 entity-manager[11384]: properties changed callback
    Nov 25 13:40:17 s8030-bmc-30303035c0c1 entity-manager[11384]: properties changed callback timer expired
    Nov 25 13:40:17 s8030-bmc-30303035c0c1 entity-manager[11384]: properties changed callback in progress
    ```
    
    Change-Id: Ic218a32180d18e2b67aa0df2aa9397e3d30be1ed
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- src/entity_manager/main.cpp
+++ src/entity_manager/main.cpp
@@ -8,6 +8,8 @@
 #include <nlohmann/json.hpp>
 #include <sdbusplus/asio/connection.hpp>
 
+#include <chrono>
+
 int main()
 {
     const std::vector<std::filesystem::path> configurationDirectories = {
@@ -17,11 +19,13 @@ int main()
 
     const std::filesystem::path configurationOutDir("/var/configuration");
 
+    const std::chrono::milliseconds propertiesChangedTimeoutMilliseconds(500);
+
     boost::asio::io_context io;
     auto systemBus = std::make_shared<sdbusplus::asio::connection>(io);
     systemBus->request_name("xyz.openbmc_project.EntityManager");
     EntityManager em(systemBus, io, configurationDirectories, schemaDirectory,
-                     configurationOutDir);
+                     configurationOutDir, propertiesChangedTimeoutMilliseconds);
 
     em.powerStatus.getInitialPowerStatus(*systemBus);
 
