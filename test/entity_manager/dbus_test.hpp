e2ac1204097fc7a7471d9eb8cd474f818dc0fd69
Alexander Hansen <alexander.hansen@9elements.com>
2025-11-21 10:21:05 +0100

    test: entity-manger: end-to-end probe true
    This patch adds an end-to-end integration test for entity-manager.
    An example configuration is written to a configuration directory, EM
    picks that up exposes the configuration due to Probe: "TRUE" statement.
    
    After EM initializes, an assertion handler is run to confirm that
    the expected configuration interface has been created on the expected
    DBus path.
    
    The testcaes represents more coverage than the preceding test for DBus
    interfaces since loading and processing of configuration is also tested.
    
    Possible follow-up works:
    
    - end-to-end testcase which does not use Probe: TRUE but an actual DBus
      interface
    
    Tested: Unit Test Pass
    
    Change-Id: I9757997b34026f3b71c83ad2f5c17e2253370a2d
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- test/entity_manager/dbus_test.hpp
+++ test/entity_manager/dbus_test.hpp
@@ -2,6 +2,7 @@
 
 #include "test_em.hpp"
 
+#include <boost/asio/steady_timer.hpp>
 #include <nlohmann/json.hpp>
 
 #include <flat_map>
@@ -36,7 +37,8 @@ class DBusTest : public testing::Test
     DBusTest& operator=(const DBusTest&) = delete;
     DBusTest& operator=(DBusTest&&) = delete;
 
-    void postAssertHandlerCallback(
+    static void postAssertHandlerCallback(
+        boost::asio::io_context& io,
         const std::function<void(const DBusPropertiesMap& value)>& handler,
         bool stopOnError, const boost::system::error_code& ec,
         const DBusPropertiesMap& value)
@@ -58,11 +60,13 @@ class DBusTest : public testing::Test
 
         try
         {
+            lg2::debug("test: running assertion handler");
             handler(value);
             io.stop();
         }
         catch (std::exception& e)
         {
+            lg2::error("caught exception: {ERR}", "ERR", e);
             if (stopOnError)
             {
                 io.stop();
@@ -72,17 +76,61 @@ class DBusTest : public testing::Test
 
     void postAssertHandler(
         const std::function<void(const DBusPropertiesMap& value)>& handler,
-        const std::string& path, const std::string& interface,
-        bool stopOnError = true)
+        const sdbusplus::message::object_path& path,
+        const std::string& interface, bool stopOnError = true,
+        const size_t timeoutMs = 1)
     {
-        std::function<void(const boost::system::error_code&,
-                           const DBusPropertiesMap&)>
-            h3 = std::bind_front(&DBusTest::postAssertHandlerCallback, this,
-                                 handler, stopOnError);
+        staticPostAssertHandler(io, systemBus, handler, em.busName, path,
+                                interface, timeoutMs, stopOnError);
+    }
 
-        systemBus->async_method_call(h3, em.busName, path,
+    static void timerHandler(
+        const std::shared_ptr<sdbusplus::asio::connection>& systemBus,
+        const std::function<void(const boost::system::error_code&,
+                                 const DBusPropertiesMap& value)>& handler,
+        const std::string& busName, const sdbusplus::message::object_path& path,
+        const std::string& interface,
+
+        const boost::system::error_code& ec)
+    {
+        lg2::debug("timer expired");
+        if (ec)
+        {
+            lg2::error("timer error");
+            return;
+        }
+        systemBus->async_method_call(handler, busName, path,
                                      "org.freedesktop.DBus.Properties",
                                      "GetAll", interface);
+    }
+
+    static void staticPostAssertHandler(
+        boost::asio::io_context& io,
+        const std::shared_ptr<sdbusplus::asio::connection>& systemBus,
+        const std::function<void(const DBusPropertiesMap& value)>& handler,
+        const std::string& busName, const sdbusplus::message::object_path& path,
+        const std::string& interface, const size_t timeoutMs,
+        bool stopOnError = true)
+    {
+        lg2::debug("test: running assertion handler or {BUS} {PATH} {INTF}",
+                   "BUS", busName, "PATH", path, "INTF", interface);
+
+        std::function<void(const boost::system::error_code&,
+                           const DBusPropertiesMap&)>
+            h3 = std::bind_front(DBusTest::postAssertHandlerCallback,
+                                 std::ref(io), handler, stopOnError);
+
+        boost::asio::steady_timer timer(io);
+        // we have to wait until properties changed is done
+
+        lg2::debug("setting timer to {VALUE} ms", "VALUE", timeoutMs);
+        timer.expires_after(std::chrono::milliseconds(timeoutMs));
+
+        std::function<void(const boost::system::error_code&)> h4 =
+            std::bind_front(timerHandler, systemBus, h3, busName, path,
+                            interface);
+
+        timer.async_wait(h4);
 
         io.run();
     };
