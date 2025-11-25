acb438f86cf636e49b7bee7192d782b784ba88ff
Alexander Hansen <alexander.hansen@9elements.com>
2025-11-25 12:21:30 +0100

    entity-manager: parameterize timeouts
    entity-manager has a bunch of internal timeouts which delay DBus
    operations to avoid computing configuration changes too often.
    
    These timeouts are completely arbitrary and can be routed out as
    parameters to the implementation instead of hardcoding.
    
    This allows for running entity-manager integration tests in a reasonable
    amount of time, since DBus operations are not delayed.
    
    Tested: TODO
    
    Change-Id: I0eaaa726c64434ff7ae6f1fc7d8ee0c94ab8fb88
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- src/entity_manager/main.cpp
+++ src/entity_manager/main.cpp
@@ -19,13 +19,20 @@ int main()
 
     const std::filesystem::path configurationOutDir("/var/configuration");
 
+    // timeouts configuration
     const std::chrono::milliseconds propertiesChangedTimeoutMilliseconds(500);
+    const std::chrono::milliseconds buildDeviceTimeoutMillis(10000);
+    const std::chrono::milliseconds startRemovedTimeoutMillis(500);
+    const std::chrono::milliseconds getInterfacesTimeoutMillis(2000);
+    const std::chrono::milliseconds findDBusObjectsTimeoutMillis(10000);
 
     boost::asio::io_context io;
     auto systemBus = std::make_shared<sdbusplus::asio::connection>(io);
     systemBus->request_name("xyz.openbmc_project.EntityManager");
     EntityManager em(systemBus, io, configurationDirectories, schemaDirectory,
-                     configurationOutDir, propertiesChangedTimeoutMilliseconds);
+                     configurationOutDir, propertiesChangedTimeoutMilliseconds,
+                     buildDeviceTimeoutMillis, startRemovedTimeoutMillis,
+                     getInterfacesTimeoutMillis, findDBusObjectsTimeoutMillis);
 
     em.powerStatus.getInitialPowerStatus(*systemBus);
 
