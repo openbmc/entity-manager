ce206cc63a163eb91197f29b00b7aa3395463f59
Alexander Hansen <alexander.hansen@9elements.com>
2025-12-04 15:29:40 +0100

    dbus_regex: introduce sanitize*InPlace
    Instead of calling `std::regex_replace` in multiple places with the same
    arguments, create a wrapper to fill those in for us.
    
    - sanitizePathInPlace
    - sanitizeMemberInPlace
    
    For DBus member/name and DBus path respectively.
    
    Resulting in more consistent usage and improving readability.
    
    Tested: Inspection only.
    
    Change-Id: I959d7ba4afe18dacdf2f091cc46c0c35fb2d35ce
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- src/dbus_regex.hpp
+++ src/dbus_regex.hpp
@@ -8,4 +8,7 @@ namespace dbus_regex
 extern const std::regex illegalDbusPathRegex;
 extern const std::regex illegalDbusMemberRegex;
 
+void sanitizeMemberInPlace(std::string& str);
+void sanitizePathInPlace(std::string& str);
+
 } // namespace dbus_regex
