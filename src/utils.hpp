83c07fdc0680aea324675d9b6f0ce5ff39fc1c10
Alexander Hansen <alexander.hansen@9elements.com>
2025-12-05 14:25:51 +0100

    deduplicate DBus variant definitions
    There were several versions of std::variant for DBus types in use
    throughout the repo to represent all the types we may get from DBus.
    
    Form the union of these variants to use in all those places.
    
    Since the variant is used in DBus context it's assumed we can safely use
    the same variant type in all cases.
    
    Tested: EM integration tests pass.
    
    Tested: on Tyan S8030
    
    `busctl tree` is as before
    
    inspecting a single record:
    
    ```
    busctl introspect xyz.openbmc_project.EntityManager /xyz/openbmc_project/inventory/system/board/Tyan_S8030_Baseboard/VBAT_33 xyz.openbmc_project.Configuration.ADC
    NAME                                  TYPE      SIGNATURE RESULT/VALUE FLAGS
    .Index                                property  t         10           emits-change
    .Name                                 property  s         "VBAT_33"    emits-change
    .PowerState                           property  s         "Always"     emits-change
    .ScaleFactor                          property  d         0.3333       emits-change
    .Type                                 property  s         "ADC"        emits-change
    ```
    
    `ipmitool sensor` also shows the expected readings, indicating sensor
    config was parsed successfully.
    
    Change-Id: I6e0185f41d2860d8370fe515dd33acde84f30f70
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- src/utils.hpp
+++ src/utils.hpp
@@ -13,7 +13,8 @@
 
 using DBusValueVariant =
     std::variant<std::string, int64_t, uint64_t, double, int32_t, uint32_t,
-                 int16_t, uint16_t, uint8_t, bool, std::vector<uint8_t>>;
+                 int16_t, uint16_t, uint8_t, bool, std::vector<uint8_t>,
+                 std::vector<std::string>>;
 using DBusInterface = std::flat_map<std::string, DBusValueVariant, std::less<>>;
 using DBusObject = std::flat_map<std::string, DBusInterface, std::less<>>;
 using MapperGetSubTreeResponse =
