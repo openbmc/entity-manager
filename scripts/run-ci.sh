ae3957452ac0475dbadb8ffa6b680a260b71176e
Patrick Williams <patrick@stwcx.xyz>
2025-12-15 16:40:39 -0500

    format: add generate scripts
    Add the generation scripts to the `format-code` helper instead of the
    `run-ci` helper.  Both are ran in Jenkins so there is no change to
    test coverage.  `format-code` is lighter weight, so more likely to be
    ran manually / locally by developers.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: Ibfaf4369d2e5519fc9441a13cc6ec82d307a1089

--- scripts/run-ci.sh
+++ scripts/run-ci.sh
@@ -3,10 +3,3 @@
 set -e
 
 scripts/validate_configs.py -v -k -e test/expected-schema-errors.txt
-
-# fail the script if someone forgot to update the list of configurations or
-# schemas
-
-scripts/generate_config_list.sh
-scripts/generate_schema_list.sh
-git --no-pager diff --exit-code -- .
