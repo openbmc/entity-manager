92ce744c241d6a25daca25b7bb2c3035366e45e3
Nikhila D <nikhilad@ami.com>
2025-12-16 22:20:19 +0530

    Add logs to fwVersionIsSame()
    Add logs to fwVersionIsSame() to indicate whether the firmware
    version is changed or not.
    
    Tested:
    Logs are printed as expected when firmware updating and BMC rebooting.
    
    Log of rebooting:
    The firmware version is similar as the last boot,
    Hash value of versionFile is:3336889560
    
    Log of Firmware update:
    The firmware version is changed since the last boot,
    hash value of current versionFile is:3336889560,
    hash value of versionFile of last boot is:834871226
    
    Change-Id: I53bf9081e9c15bd195407138d013a1d3a600bee3
    Signed-off-by: Nikhila D <nikhilad@ami.com>

--- src/entity_manager/utils.cpp
+++ src/entity_manager/utils.cpp
@@ -9,6 +9,7 @@
 
 #include <fstream>
 #include <regex>
+#include <iostream>
 
 const std::regex illegalDbusMemberRegex("[^A-Za-z0-9_]");
 
@@ -54,8 +55,17 @@ bool fwVersionIsSame()
 
         if (expectedHash == hashString)
         {
+            std::cout << "The firmware version is similiar as the last boot, "
+                         "hash value of versionFile is:"
+                      << hashString.c_str() << "\n";
             return true;
         }
+        std::cout
+            << "The firmware version is changed since the last boot, hash "
+               "value of current versionFile is:"
+            << expectedHash.c_str()
+            << ", hash value of versionFile of last boot is:"
+            << hashString.c_str() << "\n";
         hashFile.close();
     }
 
