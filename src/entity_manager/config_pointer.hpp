0e8a1f8ba5f8018edece72d69e93ce9cde9358d4
Alexander Hansen <alexander.hansen@9elements.com>
2026-01-15 05:12:09 +0100

    introduce struct ConfigPointer
    Introduce this struct to avoid using json pointers [1].
    
    json pointers are locking us into json based data-structures which are
    generally bad for readability and review purposes.
    
    If everything's a json object, it's pretty easy for things to get mixed
    up and checks for existence / correct type are required for every
    access, bloating the code.
    
    The new 'struct ConfigPointer` stores a board id and optionally other
    members like exposes index, property name, and array index, to allow
    indexing to every part of the system configuration.
    
    Tested: on Tyan S8030
    
    `busctl tree` looking normal, no obviously missing records
    
    `busctl introspect` looking normal, on normal properties, array
    properties, and object properties nested inside arrays.
    Confirmed this with a record of Type "SPIFlash"
    
    Testing the `AddObject` workflow
    
    ```
    busctl call xyz.openbmc_project.EntityManager /xyz/openbmc_project/inventory/system/board/Tyan_S8030_Baseboard xyz.openbmc_project.AddObject AddObject a{sv} 3 Name s DummyRecord Type s DummyType PollRate u 1000
    
    root@s8030-bmc-30303035c0c1:~# busctl introspect xyz.openbmc_project.EntityManager /xyz/openbmc_project/inventory/system/board/Tyan_S8030_Baseboard/DummyRecord
    NAME                                        TYPE      SIGNATURE RESULT/VALUE  FLAGS
    ...
    xyz.openbmc_project.Configuration.DummyType interface -         -             -
    .Delete                                     method    -         -             -
    .Name                                       property  s         "DummyRecord" emits-change
    .PollRate                                   property  d         1000          emits-change
    .Type                                       property  s         "DummyType"   emits-change
    ```
    
    The new record is added without issues.
    
    EM log output looks normal.
    
    Association interfaces are created as expected.
    
    References:
    
    [1] https://json.nlohmann.me/features/json_pointer/#introduction
    
    Change-Id: Idfa6ef852c5a573b63ea47f3baef9bfd96db5c74
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ src/entity_manager/config_pointer.hpp
@@ -0,0 +1,129 @@
+#pragma once
+
+#include <nlohmann/json.hpp>
+#include <phosphor-logging/lg2.hpp>
+
+#include <cstdint>
+#include <optional>
+#include <string>
+
+// This structure is used instead of a json pointer.
+// Which helps to avoid forcing us to use json data structures.
+struct ConfigPointer
+{
+  public:
+    explicit ConfigPointer(const std::string& boardIdIn);
+    explicit ConfigPointer(const std::string& boardIdIn,
+                           uint64_t exposesIndexIn);
+    explicit ConfigPointer(const std::string& boardIdIn,
+                           uint64_t exposesIndexIn,
+                           const std::string& propNameIn);
+
+    // This is for properties directly on a board config and not part of an
+    // exposes record
+    explicit ConfigPointer(const std::string& boardIdIn,
+                           const std::string& propNameIn);
+
+    // This represents indexing into a board, exposes record, config record, and
+    // array element within
+    explicit ConfigPointer(const std::string& boardIdIn,
+                           uint64_t exposesIndexIn,
+                           const std::string& propNameIn,
+                           uint64_t arrayIndexIn);
+
+    // Builder-style API (implemented as-needed)
+
+    ConfigPointer withExposesIndexAndName(uint64_t index,
+                                          const std::string& name) const;
+
+    ConfigPointer withArrayIndex(uint64_t arrayIndex) const;
+
+    ConfigPointer withName(const std::string& name) const;
+
+    // @brief writes configuration at the pointed-to location
+    // @returns false on error
+    template <typename JsonType>
+    bool write(const JsonType& value, nlohmann::json& systemConfiguration) const
+    {
+        lg2::debug("config ptr: writing value: {VALUE}", "VALUE",
+                   nlohmann::json(value));
+
+        if (!systemConfiguration.contains(boardId))
+        {
+            lg2::error("error: config ptr: board id {ID} not found", "ID",
+                       boardId);
+            return false;
+        }
+        nlohmann::json& ref = systemConfiguration.at(boardId);
+        if (!exposesIndex.has_value())
+        {
+            if (!propertyName.has_value())
+            {
+                ref = value;
+                return true;
+            }
+            ref[propertyName.value()] = value;
+            return true;
+        }
+        const uint64_t exposesIndexValue = exposesIndex.value();
+        if (!ref.contains("Exposes"))
+        {
+            lg2::error("error: config ptr: missing 'Exposes' property");
+            return false;
+        }
+        nlohmann::json& exposesJson = ref["Exposes"];
+        if (!exposesJson.is_array())
+        {
+            lg2::error("error: config ptr: 'Exposes' is not an array");
+            return false;
+        }
+        if (exposesJson.size() <= exposesIndexValue)
+        {
+            lg2::error("error: config ptr: exposes index {INDEX} out of bounds",
+                       "INDEX", exposesIndexValue);
+            return false;
+        }
+        nlohmann::json& record = exposesJson[exposesIndexValue];
+        if (!propertyName.has_value())
+        {
+            record = value;
+            return true;
+        }
+        const std::string& propNameValue = propertyName.value();
+        if (!record.contains(propNameValue))
+        {
+            lg2::error("error: config ptr: could not set property {NAME}",
+                       "NAME", propNameValue);
+            return false;
+        }
+        if (!arrayIndex.has_value())
+        {
+            record[propNameValue] = value;
+            return true;
+        }
+        const uint64_t arrayIndexValue = arrayIndex.value();
+        if (!record[propNameValue].is_array())
+        {
+            lg2::error("error: config ptr: property {NAME} is not an array",
+                       "NAME", propNameValue);
+            return false;
+        }
+        nlohmann::json& arrayJson = record[propNameValue];
+        if (arrayJson.size() <= arrayIndexValue)
+        {
+            lg2::error("error: config ptr: array index {INDEX} out of bounds",
+                       "INDEX", arrayIndexValue);
+            return false;
+        }
+        record[propNameValue][arrayIndexValue] = value;
+        return true;
+    }
+
+    // This will be an hash (integer) in almost all cases
+    std::string boardId;
+
+    std::optional<uint64_t> exposesIndex = std::nullopt;
+    std::optional<std::string> propertyName = std::nullopt;
+
+    std::optional<uint64_t> arrayIndex = std::nullopt;
+};
