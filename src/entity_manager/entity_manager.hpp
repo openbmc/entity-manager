4887bbad272d1a3b8d0908f99cc1ecbb64ce1390
Alexander Hansen <alexander.hansen@9elements.com>
2026-01-15 05:12:09 +0100

    introduce struct ConfigPointer
    Introduce this struct to avoid using json pointers [1].
    
    json pointers are locking us into json based data-structures which are
    generally bad for readability and review purposes.
    
    If everything's a json object, it's pretty easy for things to get mixed
    up and checks for existence / correct type are required for every
    access, bloating the code.
    
    The new 'struct ConfigPointer` stores a board id and optionally other
    members like exposes index, property name, and array index, to allow
    indexing to every part of the system configuration.
    
    Tested: TODO
    
    References:
    
    [1] https://json.nlohmann.me/features/json_pointer/#introduction
    
    Change-Id: Idfa6ef852c5a573b63ea47f3baef9bfd96db5c74
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- src/entity_manager/entity_manager.hpp
+++ src/entity_manager/entity_manager.hpp
@@ -58,15 +58,14 @@ class EntityManager
                          std::map<std::string, std::string>& newBoards);
     void postExposesRecordsToDBus(
         nlohmann::json& item, size_t& exposesIndex,
-        const std::string& boardNameOrig, std::string jsonPointerPath,
-        const std::string& jsonPointerPathBoard, const std::string& boardPath,
-        const std::string& boardType);
+        const std::string& boardNameOrig, const std::string& boardId,
+        const std::string& boardPath, const std::string& boardType);
 
     // @returns false on error
     bool postConfigurationRecord(
         const std::string& name, nlohmann::json& config,
         const std::string& boardNameOrig, const std::string& itemType,
-        const std::string& jsonPointerPath, const std::string& ifacePath);
+        const ConfigPointer& configPtr, const std::string& ifacePath);
 
     void pruneConfiguration(bool powerOff, const std::string& name,
                             const nlohmann::json& device);
