openbmc/entity-manager

commit 1cd0b0440109eb9cd7c0f0c842eae1b518649352

fru-device: use ioctl to write fru for 16-bit address
smbus commands only support eeproms with 8-bit address [1]. this patch
checks device address type and uses ioctl if uses 16-bit addressing.

tested on 24AA64 eeprom.

[1] https://docs.kernel.org/i2c/smbus-protocol.html -> command byte is
only 8 bits.

Change-Id: Iff999acab7c8d04757826a389af3e5f19759bd0b
Signed-off-by: Robin Cheng <rocheng@nvidia.com>

src/fru_device/fru_device.cpp
@@ -1003,6 +1003,39 @@ static bool readBaseboardFRU(std::vector<uint8_t>& baseboardFRU)
     return true;
 }
 
+bool writeFruByteData(bool is16Bit, int file, uint8_t address, uint16_t index,
+                      uint8_t byteData)
+{
+    if (is16Bit)
+    {
+        // if uses 16-bit addressing we need to use ioctl as smbus commands
+        // are not supported
+        struct i2c_rdwr_ioctl_data messagesData = {};
+        std::array<struct i2c_msg, 1> messages{};
+        std::array<uint8_t, 3> writeBuffer{};
+
+        uint8_t indexH = index >> 8;
+        uint8_t indexL = index & 0xFF;
+
+        writeBuffer[0] = indexH;
+        writeBuffer[1] = indexL;
+        writeBuffer[2] = byteData;
+
+        messages[0].flags = 0;
+        messages[0].len = writeBuffer.size();
+        messages[0].buf = writeBuffer.data();
+        messages[0].addr = address;
+
+        messagesData.msgs = messages.data();
+        messagesData.nmsgs = 1;
+
+        return ioctl(file, I2C_RDWR, &messagesData) >= 0;
+    }
+    // if 8-bit addressing
+    return i2c_smbus_write_byte_data(file, static_cast<uint8_t>(index),
+                                     byteData) == 0;
+}
+
 bool writeFRU(uint8_t bus, uint8_t address, const std::vector<uint8_t>& fru)
 {
     std::flat_map<std::string, std::string, std::less<>> tmp;
@@ -1102,6 +1135,13 @@ bool writeFRU(uint8_t bus, uint8_t address, const std::vector<uint8_t>& fru)
         return false;
     }
 
+    std::optional<bool> is16Bit = isDevice16Bit(file, address);
+    if (!is16Bit.has_value())
+    {
+        std::cerr << "failed to detect if device is 8 or 16 bits" << std::endl;
+        return false;
+    }
+
     constexpr const size_t retryMax = 2;
     uint16_t index = 0;
     size_t retries = retryMax;
@@ -1121,8 +1161,12 @@ bool writeFRU(uint8_t bus, uint8_t address, const std::vector<uint8_t>& fru)
             }
         }
 
-        if (i2c_smbus_write_byte_data(file, static_cast<uint8_t>(index),
-                                      fru[index]) < 0)
+        if (writeFruByteData(*is16Bit, file, address, index, fru[index]))
+        {
+            retries = retryMax;
+            index++;
+        }
+        else
         {
             if ((retries--) == 0U)
             {
@@ -1132,11 +1176,7 @@ bool writeFRU(uint8_t bus, uint8_t address, const std::vector<uint8_t>& fru)
                 return false;
             }
         }
-        else
-        {
-            retries = retryMax;
-            index++;
-        }
+
         // most eeproms require 5-10ms between writes
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }

commit 4276d4ad645dd71b7dbc22b1c4f5871957971b57

fru-device: use lg2::error instead of cerr
An old commit was rebased and applied without testing.  It does not
compile due to other refactoring that migrated from cerr to lg2.
Adjust the corresponding log statement in fru-device.

Fixes: 1cd0b0440109 ("fru-device: use ioctl to write fru for 16-bit address")
Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
Change-Id: I465624f40f553f184b53294d5c355aff28d238e9

src/fru_device/fru_device.cpp
@@ -1138,7 +1138,7 @@ bool writeFRU(uint8_t bus, uint8_t address, const std::vector<uint8_t>& fru)
     std::optional<bool> is16Bit = isDevice16Bit(file, address);
     if (!is16Bit.has_value())
     {
-        std::cerr << "failed to detect if device is 8 or 16 bits" << std::endl;
+        lg2::error("failed to detect if device is 8 or 16 bits");
         return false;
     }
 