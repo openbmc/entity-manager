635e9c9fb108c2749592053ce9efcd5a997e2280
Alexander Hansen <alexander.hansen@9elements.com>
2026-03-20 13:13:57 +0100

    optimize DBus path/interface sanitizer
    Change the implementation from std::regex_replace to
    std::ranges::replace_if and loop based solutions for better performance.
    
    This has not been benchmarked in any way, but i don't see regex being
    more efficient than a simple loop and review feedback has suggested this
    change [1].
    
    Rename the functions to reflect their actual purpose, the names
    have been wrong (swapped) for a while now.
    
    Create documentation comments and align the implementation with D-Bus
    spec.
    
    Tested: new and existing unit tests are passing.
    
    Tested: on Tyan S5549
    
    - busctl tree looks normal, object paths as expected
    - busctl introspect looks normal, interface names as expected
    
    References:
    [1] https://gerrit.openbmc.org/c/openbmc/entity-manager/+/85783/comment/b429ac20_f7a9b0b2/
    [2] 9cf19230e11a ("Refactor dbus regexes")
    [3] 628b886a320b ("dbus_regex: sanitizeForDBus(Path|Member)")
    
    Change-Id: I6a51c36342877194e89b8da0e0e1970a2f23c836
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- test/test_dbus_util.cpp
+++ test/test_dbus_util.cpp
@@ -2,50 +2,90 @@
 
 #include <gtest/gtest.h>
 
-TEST(DBUS_UTIL, SanitizePathNop)
+TEST(DBUS_UTIL, SanitizeIntfEmpty)
 {
-    std::string path = "Tyan_S8030_Baseboard";
-    path = dbus_util::sanitizeForDBusPath(path);
+    const std::string intf;
 
-    ASSERT_EQ(path, "Tyan_S8030_Baseboard");
+    ASSERT_EQ(dbus_util::sanitizeForDBusInterface(intf), "_");
 }
 
-TEST(DBUS_UTIL, SanitizePathSimple)
+TEST(DBUS_UTIL, SanitizeIntfSegmentNotBeginWithDigit)
 {
-    std::string path = "Tyan S8030 Baseboard";
-    path = dbus_util::sanitizeForDBusPath(path);
+    const std::string intf = "9.Stepwise";
 
-    ASSERT_EQ(path, "Tyan_S8030_Baseboard");
+    ASSERT_EQ(dbus_util::sanitizeForDBusInterface(intf), "_.Stepwise");
 }
 
-TEST(DBUS_UTIL, SanitizePathWithDot)
+TEST(DBUS_UTIL, SanitizeIntfSegmentNotBeginWithDigit2)
 {
-    std::string path = "xyz.openbmc_project.Configuration.ADC";
-    path = dbus_util::sanitizeForDBusPath(path);
+    const std::string intf = "PID.9";
 
-    ASSERT_EQ(path, "xyz.openbmc_project.Configuration.ADC");
+    ASSERT_EQ(dbus_util::sanitizeForDBusInterface(intf), "PID._");
 }
 
-TEST(DBUS_UTIL, SanitizeMemberNop)
+TEST(DBUS_UTIL, SanitizeIntfEmptySegment)
 {
-    std::string path = "P0_VDD_MEM_ABCD";
-    path = dbus_util::sanitizeForDBusMember(path);
+    const std::string intf = "..";
 
-    ASSERT_EQ(path, "P0_VDD_MEM_ABCD");
+    ASSERT_EQ(dbus_util::sanitizeForDBusInterface(intf), "_._");
 }
 
-TEST(DBUS_UTIL, SanitizeMember)
+TEST(DBUS_UTIL, SanitizeIntfEmptySegmentEnd)
 {
-    std::string path = "P0 VDD bus % 13";
-    path = dbus_util::sanitizeForDBusMember(path);
+    const std::string intf = "Configuration.";
 
-    ASSERT_EQ(path, "P0_VDD_bus___13");
+    ASSERT_EQ(dbus_util::sanitizeForDBusInterface(intf), "Configuration._");
 }
 
-TEST(DBUS_UTIL, SanitizeMemberWithDot)
+TEST(DBUS_UTIL, SanitizeIntfNop)
 {
-    std::string path = "xyz.openbmc_project.Configuration.ADC";
-    path = dbus_util::sanitizeForDBusMember(path);
+    const std::string intf = "Tyan_S8030_Baseboard";
 
-    ASSERT_EQ(path, "xyz_openbmc_project_Configuration_ADC");
+    ASSERT_EQ(dbus_util::sanitizeForDBusInterface(intf),
+              "Tyan_S8030_Baseboard");
+}
+
+TEST(DBUS_UTIL, SanitizeIntfNop2)
+{
+    const std::string intf = "Pid.Zone";
+
+    ASSERT_EQ(dbus_util::sanitizeForDBusInterface(intf), "Pid.Zone");
+}
+
+TEST(DBUS_UTIL, SanitizeIntfSimple)
+{
+    const std::string intf = "Tyan S8030 Baseboard";
+
+    ASSERT_EQ(dbus_util::sanitizeForDBusInterface(intf),
+              "Tyan_S8030_Baseboard");
+}
+
+TEST(DBUS_UTIL, SanitizeIntfWithDot)
+{
+    const std::string intf = "xyz.openbmc_project.Configuration.ADC";
+
+    ASSERT_EQ(dbus_util::sanitizeForDBusInterface(intf),
+              "xyz.openbmc_project.Configuration.ADC");
+}
+
+TEST(DBUS_UTIL, SanitizePathSegmentNop)
+{
+    const std::string path = "P0_VDD_MEM_ABCD";
+
+    ASSERT_EQ(dbus_util::sanitizeForDBusPathSegment(path), "P0_VDD_MEM_ABCD");
+}
+
+TEST(DBUS_UTIL, SanitizePathSegment)
+{
+    const std::string path = "P0 VDD bus % 13";
+
+    ASSERT_EQ(dbus_util::sanitizeForDBusPathSegment(path), "P0_VDD_bus___13");
+}
+
+TEST(DBUS_UTIL, SanitizePathSegmentWithDot)
+{
+    const std::string path = "xyz.openbmc_project.Configuration.ADC";
+
+    ASSERT_EQ(dbus_util::sanitizeForDBusPathSegment(path),
+              "xyz_openbmc_project_Configuration_ADC");
 }
