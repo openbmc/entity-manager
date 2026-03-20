openbmc/entity-manager

commit 628b886a320be9ec7bf9e5107843afbf3f2aed93

dbus_regex: sanitizeForDBus(Path|Member)
Instead of calling `std::regex_replace` in multiple places with the same
arguments, create a wrapper to fill those in for us.

- sanitizeForDBusPath
- sanitizeForDBusMember

For DBus member/name and DBus path respectively.

Resulting in more consistent usage and improving readability.

Tested: on Tyan S8030

object path
`/xyz/openbmc_project/inventory/system/board/Tyan_S8030_Baseboard`
is correctly sanitized and spaces were replaced with underscores.

Looking at `busctl tree`, no object path appears out of place.

For FruDevice, product name also appears as before.

```
busctl introspect xyz.openbmc_project.FruDevice /xyz/openbmc_project/FruDevice/PWS_920P_SQ xyz.openbmc_project.FruDevice
NAME                          TYPE      SIGNATURE RESULT/VALUE      FLAGS
.UpdateFruField               method    ss        b                 -
.ADDRESS                      property  u         56                emits-change
.BUS                          property  u         3                 emits-change
.Common_Format_Version        property  s         "1"               emits-change
.PRODUCT_LANGUAGE_CODE        property  s         "0"               emits-change
.PRODUCT_MANUFACTURER         property  s         "SUPERMICRO"      emits-change writable
.PRODUCT_PART_NUMBER          property  s         "PWS-920P-SQ"     emits-change writable
.PRODUCT_PRODUCT_NAME         property  s         "PWS-920P-SQ"     emits-change writable
.PRODUCT_SERIAL_NUMBER        property  s         "P9212CK04EN1230" emits-change writable
.PRODUCT_VERSION              property  s         "1.3"             emits-change writable
```

Change-Id: I959d7ba4afe18dacdf2f091cc46c0c35fb2d35ce
Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

src/entity_manager/dbus_interface.cpp
@@ -8,7 +8,6 @@
 
 #include <flat_map>
 #include <fstream>
-#include <regex>
 #include <string>
 #include <vector>
 
@@ -353,10 +352,8 @@ void EMDBusInterface::addObjectJson(
     {
         lg2::error("Error writing json files");
     }
-    std::string dbusName = *name;
 
-    std::regex_replace(dbusName.begin(), dbusName.begin(), dbusName.end(),
-                       dbus_regex::illegalDbusMemberRegex, "_");
+    std::string dbusName = dbus_regex::sanitizeForDBusMember(*name);
 
     std::shared_ptr<sdbusplus::asio::dbus_interface> interface =
         createInterface(path + "/" + dbusName,

src/entity_manager/entity_manager.cpp
@@ -143,10 +143,8 @@ void EntityManager::postBoardToDBus(
     if (findBoardType != boardValues.end() &&
         findBoardType->type() == nlohmann::json::value_t::string)
     {
-        boardType = findBoardType->get<std::string>();
-        std::regex_replace(boardType.begin(), boardType.begin(),
-                           boardType.end(), dbus_regex::illegalDbusMemberRegex,
-                           "_");
+        boardType = dbus_regex::sanitizeForDBusMember(
+            findBoardType->get<std::string>());
     }
     else
     {
@@ -243,20 +241,16 @@ void EntityManager::postExposesRecordsToDBus(
         }
     }
     auto findType = item.find("Type");
-    std::string itemType;
+    std::string itemType = "unknown";
     if (findType != item.end())
     {
-        itemType = findType->get<std::string>();
-        std::regex_replace(itemType.begin(), itemType.begin(), itemType.end(),
-                           dbus_regex::illegalDbusPathRegex, "_");
+        itemType =
+            dbus_regex::sanitizeForDBusPath(findType->get<std::string>());
     }
-    else
-    {
-        itemType = "unknown";
-    }
-    std::string itemName = findName->get<std::string>();
-    std::regex_replace(itemName.begin(), itemName.begin(), itemName.end(),
-                       dbus_regex::illegalDbusMemberRegex, "_");
+
+    const std::string itemName =
+        dbus_regex::sanitizeForDBusMember(findName->get<std::string>());
+
     std::string ifacePath = boardPath;
     ifacePath += "/";
     ifacePath += itemName;

src/entity_manager/overlay.cpp
@@ -18,7 +18,6 @@
 #include <flat_map>
 #include <fstream>
 #include <iomanip>
-#include <regex>
 #include <string>
 
 constexpr const char* outputDir = "/tmp/overlays";
@@ -227,9 +226,8 @@ void exportDevice(const devices::ExportTemplate& exportTemplate,
         if (keyPair.key() == "Name" &&
             keyPair.value().type() == nlohmann::json::value_t::string)
         {
-            subsituteString =
-                std::regex_replace(keyPair.value().get<std::string>(),
-                                   dbus_regex::illegalDbusMemberRegex, "_");
+            subsituteString = dbus_regex::sanitizeForDBusMember(
+                keyPair.value().get<std::string>());
             name = subsituteString;
         }
         else

src/entity_manager/utils.cpp
@@ -342,8 +342,8 @@ std::string buildInventorySystemPath(std::string& boardName,
 {
     std::string path = "/xyz/openbmc_project/inventory/system/";
     std::string boardTypeLower = toLowerCopy(boardType);
-    std::regex_replace(boardName.begin(), boardName.begin(), boardName.end(),
-                       dbus_regex::illegalDbusMemberRegex, "_");
+
+    boardName = dbus_regex::sanitizeForDBusMember(boardName);
 
     return std::format("{}{}/{}", path, boardTypeLower, boardName);
 }

src/fru_device/fru_utils.cpp
@@ -1671,9 +1671,8 @@ std::optional<std::string> getProductName(
     if (productNameFind != formattedFRU.end() &&
         !productNameFind->second.empty())
     {
-        productName = productNameFind->second;
-        productName = std::regex_replace(
-            productName, dbus_regex::illegalDbusMemberRegex, "_");
+        productName =
+            dbus_regex::sanitizeForDBusMember(productNameFind->second);
     }
     else
     {

test/meson.build
@@ -78,6 +78,17 @@ test(
 )
 
 test(
+    'test_dbus_regex',
+    executable(
+        'test_dbus_regex',
+        'test_dbus_regex.cpp',
+        '../src/dbus_regex.cpp',
+        include_directories: test_include_dir,
+        dependencies: [gtest],
+    ),
+)
+
+test(
     'test_gpio_presence_state',
     executable(
         'test_gpio_presence_state',

src/dbus_regex.cpp
@@ -5,7 +5,16 @@
 namespace dbus_regex
 {
 
-const std::regex illegalDbusPathRegex("[^A-Za-z0-9_.]");
-const std::regex illegalDbusMemberRegex("[^A-Za-z0-9_]");
+std::string sanitizeForDBusMember(const std::string& str)
+{
+    static const std::regex illegalDbusMemberRegex("[^A-Za-z0-9_]");
+    return std::regex_replace(str, illegalDbusMemberRegex, "_");
+}
+
+std::string sanitizeForDBusPath(const std::string& str)
+{
+    static const std::regex illegalDbusPathRegex("[^A-Za-z0-9_.]");
+    return std::regex_replace(str, illegalDbusPathRegex, "_");
+}
 
 } // namespace dbus_regex

src/dbus_regex.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <regex>
+#include <string>
 
 namespace dbus_regex
 {
 
-extern const std::regex illegalDbusPathRegex;
-extern const std::regex illegalDbusMemberRegex;
+std::string sanitizeForDBusMember(const std::string& str);
+std::string sanitizeForDBusPath(const std::string& str);
 
 } // namespace dbus_regex

commit 4661c2d5f5e4e92d5ddcd4a93213e66cd1daaa11

rename namespace dbus_regex -> dbus_util
The functions

- sanitizeForDBusPath
- sanitizeForDBusMember

currently use regex, but that's an implementation detail. Rename the
namespace to reflect it's purpose.

Tested: grep for both namespaces, confirm no conflict with existing
namespaces inside the openbmc org. bmcweb uses "namespace dbus_utils"
but that's it [1].

Fixes: 628b886a320b ("dbus_regex: sanitizeForDBus(Path|Member)")

References:
[1] https://github.com/search?q=org%3Aopenbmc+%22namespace+dbus_util%22&type=code

Change-Id: Ie6de0e86746a60963a7d944876451090fdf945c5
Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

src/dbus_util.cpp
@@ -1,8 +1,8 @@
-#include "dbus_regex.hpp"
+#include "dbus_util.hpp"
 
 #include <regex>
 
-namespace dbus_regex
+namespace dbus_util
 {
 
 std::string sanitizeForDBusMember(const std::string& str)
@@ -17,4 +17,4 @@ std::string sanitizeForDBusPath(const std::string& str)
     return std::regex_replace(str, illegalDbusPathRegex, "_");
 }
 
-} // namespace dbus_regex
+} // namespace dbus_util

src/dbus_util.hpp
@@ -2,10 +2,10 @@
 
 #include <string>
 
-namespace dbus_regex
+namespace dbus_util
 {
 
 std::string sanitizeForDBusMember(const std::string& str);
 std::string sanitizeForDBusPath(const std::string& str);
 
-} // namespace dbus_regex
+} // namespace dbus_util

src/entity_manager/dbus_interface.cpp
@@ -1,6 +1,6 @@
 #include "dbus_interface.hpp"
 
-#include "../dbus_regex.hpp"
+#include "../dbus_util.hpp"
 #include "perform_probe.hpp"
 #include "utils.hpp"
 
@@ -353,7 +353,7 @@ void EMDBusInterface::addObjectJson(
         lg2::error("Error writing json files");
     }
 
-    std::string dbusName = dbus_regex::sanitizeForDBusMember(*name);
+    std::string dbusName = dbus_util::sanitizeForDBusMember(*name);
 
     std::shared_ptr<sdbusplus::asio::dbus_interface> interface =
         createInterface(path + "/" + dbusName,

src/entity_manager/entity_manager.cpp
@@ -3,7 +3,7 @@
 
 #include "entity_manager.hpp"
 
-#include "../dbus_regex.hpp"
+#include "../dbus_util.hpp"
 #include "../utils.hpp"
 #include "../variant_visitors.hpp"
 #include "configuration.hpp"
@@ -143,8 +143,8 @@ void EntityManager::postBoardToDBus(
     if (findBoardType != boardValues.end() &&
         findBoardType->type() == nlohmann::json::value_t::string)
     {
-        boardType = dbus_regex::sanitizeForDBusMember(
-            findBoardType->get<std::string>());
+        boardType =
+            dbus_util::sanitizeForDBusMember(findBoardType->get<std::string>());
     }
     else
     {
@@ -244,12 +244,11 @@ void EntityManager::postExposesRecordsToDBus(
     std::string itemType = "unknown";
     if (findType != item.end())
     {
-        itemType =
-            dbus_regex::sanitizeForDBusPath(findType->get<std::string>());
+        itemType = dbus_util::sanitizeForDBusPath(findType->get<std::string>());
     }
 
     const std::string itemName =
-        dbus_regex::sanitizeForDBusMember(findName->get<std::string>());
+        dbus_util::sanitizeForDBusMember(findName->get<std::string>());
 
     std::string ifacePath = boardPath;
     ifacePath += "/";

src/entity_manager/overlay.cpp
@@ -3,7 +3,7 @@
 
 #include "overlay.hpp"
 
-#include "../dbus_regex.hpp"
+#include "../dbus_util.hpp"
 #include "../utils.hpp"
 #include "devices.hpp"
 #include "utils.hpp"
@@ -226,7 +226,7 @@ void exportDevice(const devices::ExportTemplate& exportTemplate,
         if (keyPair.key() == "Name" &&
             keyPair.value().type() == nlohmann::json::value_t::string)
         {
-            subsituteString = dbus_regex::sanitizeForDBusMember(
+            subsituteString = dbus_util::sanitizeForDBusMember(
                 keyPair.value().get<std::string>());
             name = subsituteString;
         }

src/entity_manager/utils.cpp
@@ -1,6 +1,6 @@
 #include "utils.hpp"
 
-#include "../dbus_regex.hpp"
+#include "../dbus_util.hpp"
 #include "../utils.hpp"
 #include "../variant_visitors.hpp"
 #include "expression.hpp"
@@ -343,7 +343,7 @@ std::string buildInventorySystemPath(std::string& boardName,
     std::string path = "/xyz/openbmc_project/inventory/system/";
     std::string boardTypeLower = toLowerCopy(boardType);
 
-    boardName = dbus_regex::sanitizeForDBusMember(boardName);
+    boardName = dbus_util::sanitizeForDBusMember(boardName);
 
     return std::format("{}{}/{}", path, boardTypeLower, boardName);
 }

src/fru_device/fru_utils.cpp
@@ -3,7 +3,7 @@
 
 #include "fru_utils.hpp"
 
-#include "../dbus_regex.hpp"
+#include "../dbus_util.hpp"
 #include "gzip_utils.hpp"
 
 #include <phosphor-logging/lg2.hpp>
@@ -1671,8 +1671,7 @@ std::optional<std::string> getProductName(
     if (productNameFind != formattedFRU.end() &&
         !productNameFind->second.empty())
     {
-        productName =
-            dbus_regex::sanitizeForDBusMember(productNameFind->second);
+        productName = dbus_util::sanitizeForDBusMember(productNameFind->second);
     }
     else
     {

src/meson.build
@@ -5,7 +5,7 @@ installdir = join_paths(get_option('libexecdir'), 'entity-manager')
 utils_lib = static_library(
     'utils',
     'utils.cpp',
-    'dbus_regex.cpp',
+    'dbus_util.cpp',
     dependencies: [boost, phosphor_logging_dep, sdbusplus],
 )
 

test/meson.build
@@ -78,11 +78,11 @@ test(
 )
 
 test(
-    'test_dbus_regex',
+    'test_dbus_util',
     executable(
-        'test_dbus_regex',
-        'test_dbus_regex.cpp',
-        '../src/dbus_regex.cpp',
+        'test_dbus_util',
+        'test_dbus_util.cpp',
+        '../src/dbus_util.cpp',
         include_directories: test_include_dir,
         dependencies: [gtest],
     ),

test/test_dbus_util.cpp
@@ -1,51 +1,51 @@
-#include "dbus_regex.hpp"
+#include "dbus_util.hpp"
 
 #include <gtest/gtest.h>
 
-TEST(DBUS_REGEX, SanitizePathNop)
+TEST(DBUS_UTIL, SanitizePathNop)
 {
     std::string path = "Tyan_S8030_Baseboard";
-    path = dbus_regex::sanitizeForDBusPath(path);
+    path = dbus_util::sanitizeForDBusPath(path);
 
     ASSERT_EQ(path, "Tyan_S8030_Baseboard");
 }
 
-TEST(DBUS_REGEX, SanitizePathSimple)
+TEST(DBUS_UTIL, SanitizePathSimple)
 {
     std::string path = "Tyan S8030 Baseboard";
-    path = dbus_regex::sanitizeForDBusPath(path);
+    path = dbus_util::sanitizeForDBusPath(path);
 
     ASSERT_EQ(path, "Tyan_S8030_Baseboard");
 }
 
-TEST(DBUS_REGEX, SanitizePathWithDot)
+TEST(DBUS_UTIL, SanitizePathWithDot)
 {
     std::string path = "xyz.openbmc_project.Configuration.ADC";
-    path = dbus_regex::sanitizeForDBusPath(path);
+    path = dbus_util::sanitizeForDBusPath(path);
 
     ASSERT_EQ(path, "xyz.openbmc_project.Configuration.ADC");
 }
 
-TEST(DBUS_REGEX, SanitizeMemberNop)
+TEST(DBUS_UTIL, SanitizeMemberNop)
 {
     std::string path = "P0_VDD_MEM_ABCD";
-    path = dbus_regex::sanitizeForDBusMember(path);
+    path = dbus_util::sanitizeForDBusMember(path);
 
     ASSERT_EQ(path, "P0_VDD_MEM_ABCD");
 }
 
-TEST(DBUS_REGEX, SanitizeMember)
+TEST(DBUS_UTIL, SanitizeMember)
 {
     std::string path = "P0 VDD bus % 13";
-    path = dbus_regex::sanitizeForDBusMember(path);
+    path = dbus_util::sanitizeForDBusMember(path);
 
     ASSERT_EQ(path, "P0_VDD_bus___13");
 }
 
-TEST(DBUS_REGEX, SanitizeMemberWithDot)
+TEST(DBUS_UTIL, SanitizeMemberWithDot)
 {
     std::string path = "xyz.openbmc_project.Configuration.ADC";
-    path = dbus_regex::sanitizeForDBusMember(path);
+    path = dbus_util::sanitizeForDBusMember(path);
 
     ASSERT_EQ(path, "xyz_openbmc_project_Configuration_ADC");
 }