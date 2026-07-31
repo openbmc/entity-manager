openbmc/entity-manager

commit 51b7b9737493f4b3b2cd2427cf955eb579b5b263

extract function templateCharReplaceStr
Also rewrite the template char replacement machinery to use a reference
for the string we are replacing instead of pointers, removing pointer
invalidation / nullptr bug source.

This change is required to be able to do template char replacement on
strings and not have to wrap a string in json and unwrap it again to do
the replacement. It will be useful for change [1] but also stands on its
own.

Tested: new and existing unit tests pass

Tested: on anacapa qemu

string template replacement working in the extra interfaces

```
root@anacapa:~# busctl introspect xyz.openbmc_project.EntityManager /xyz/openbmc_project/inventory/system/board/BRB_L
NAME                          TYPE      SIGNATURE RESULT/VALUE                   FLAGS
xyz.openbmc_project.Inventor… interface -         -                              -
.BuildDate                    property  s         "20260326T231600Z"             emits-change
.Manufacturer                 property  s         "Inventec"                     emits-change
.Model                        property  s         "Anacapa QEMU LBB EVT2"        emits-change
.PartNumber                   property  t         0                              emits-change
.SerialNumber                 property  t         0                              emits-change
.SparePartNumber              property  s         ""                             emits-change
```

template var replacement and expression calculation working in
`BRB_L_INLET_BE3_TEMP_C` `Bus` property, calculating `$bus + 1`

```
root@anacapa:~# busctl get-property xyz.openbmc_project.EntityManager /xyz/openbmc_project/inventory/system/board/BRB_L xyz.openbmc_project.Association.Definitions Associations
a(sss) 1 "probing" "probed_by" "/xyz/openbmc_project/FruDevice/Anacapa_QEMU_LBB_EVT2"

root@anacapa:~# busctl get-property xyz.openbmc_project.EntityManager /xyz/openbmc_project/inventory/system/board/BRB_L xyz.openbmc_project.Inventory.Decorator.Asset Model

src/entity_manager/utils.cpp
@@ -182,59 +182,57 @@ std::optional<std::string> templateCharReplace(
     return std::nullopt;
 }
 
-static std::optional<bool> templateCharReplaceOneProperty(
-    std::string*& strPtr, const std::string& propName,
-    const DBusValueVariant& propValue, std::optional<std::string>& ret,
-    nlohmann::json& value)
+static bool templateCharReplaceOneProperty(
+    std::string& str, const std::string& propName,
+    const DBusValueVariant& propValue, std::optional<std::string>& ret)
 {
     std::string templateName = templateChar + propName;
     std::ranges::subrange<std::string::const_iterator> find =
-        iFindFirst(*strPtr, templateName);
+        iFindFirst(str, templateName);
     if (!find)
     {
-        return std::nullopt;
+        return false;
     }
 
-    size_t start = find.begin() - strPtr->begin();
+    size_t start = find.begin() - str.begin();
 
     // check for additional operations
-    if ((start == 0U) && find.end() == strPtr->end())
+    if ((start == 0U) && find.end() == str.end())
     {
-        std::visit([&](auto&& val) { value = val; }, propValue);
+        str = std::visit(VariantToStringVisitor(), propValue);
         return true;
     }
 
     constexpr const std::array<char, 5> mathChars = {'+', '-', '%', '*', '/'};
     size_t nextItemIdx = start + templateName.size() + 1;
 
-    if (nextItemIdx > strPtr->size() ||
-        std::find(mathChars.begin(), mathChars.end(),
-                  strPtr->at(nextItemIdx)) == mathChars.end())
+    if (nextItemIdx > str.size() ||
+        std::find(mathChars.begin(), mathChars.end(), str.at(nextItemIdx)) ==
+            mathChars.end())
     {
         std::string val = std::visit(VariantToStringVisitor(), propValue);
-        iReplaceAll(*strPtr, templateName, val);
-        return std::nullopt;
+        iReplaceAll(str, templateName, val);
+        return false;
     }
 
     // save the prefix
-    std::string prefix = strPtr->substr(0, start);
+    std::string prefix = str.substr(0, start);
 
     // operate on the rest
-    std::string end = strPtr->substr(nextItemIdx);
+    std::string end = str.substr(nextItemIdx);
 
     std::vector<std::string> splitResult = split(end, ' ');
 
     // need at least 1 operation and number
     if (splitResult.size() < 2)
     {
-        lg2::error("Syntax error on template replacement of {STR}", "STR",
-                   *strPtr);
+        lg2::error("Syntax error on template replacement of {STR}", "STR", str);
         for (const std::string& data : splitResult)
         {
             lg2::error("{SPLIT} ", "SPLIT", data);
         }
         lg2::error("");
-        return std::nullopt;
+        return false;
     }
 
     // we assume that the replacement is a number, because we can
@@ -258,33 +256,22 @@ static std::optional<bool> templateCharReplaceOneProperty(
     {
         result.append(" ").append(*exprEnd++);
     }
-    value = result;
+    str = result;
 
-    // We probably just invalidated the pointer abovei,
-    // reset and continue to handle multiple templates
-    strPtr = value.get_ptr<std::string*>();
-    if (strPtr == nullptr)
-    {
-        return false;
-    }
-
-    return std::nullopt;
+    return false;
 }
 
-static bool templateCharReplaceLoop(
-    std::string*& strPtr, const DBusInterface& interface,
-    std::optional<std::string>& ret, nlohmann::json& value)
+static void templateCharReplaceLoop(std::string& str,
+                                    const DBusInterface& interface,
+                                    std::optional<std::string>& ret)
 {
     for (const auto& [propName, propValue] : interface)
     {
-        std::optional<bool> res = templateCharReplaceOneProperty(
-            strPtr, propName, propValue, ret, value);
-        if (res.has_value())
+        if (templateCharReplaceOneProperty(str, propName, propValue, ret))
         {
-            return res.value();
+            return;
         }
     }
-    return false;
 }
 
 static void templateCharReplaceObj(
@@ -331,6 +318,23 @@ static std::optional<uint64_t> parseAsNumber(std::string_view strView)
     return std::nullopt;
 }
 
+static std::optional<std::string> templateCharReplaceStr(
+    std::string& str, const DBusInterface& interface, const size_t index,
+    const std::optional<std::string>& replaceStr)
+{
+    std::optional<std::string> ret = std::nullopt;
+
+    if (replaceStr)
+    {
+        replaceAll(str, *replaceStr, std::to_string(index));
+    }
+
+    templateCharReplaceOneProperty(str, "index", index, ret);
+    templateCharReplaceLoop(str, interface, ret);
+
+    return ret;
+}
+
 // finds the template character (currently set to $) and replaces the value with
 // the field found in a dbus object i.e. $ADDRESS would get populated with the
 // ADDRESS field from a object on dbus
@@ -355,34 +359,25 @@ std::optional<std::string> templateCharReplace(
         return ret;
     }
 
-    std::string* strPtr = value.get_ptr<std::string*>();
+    const std::string* strPtr = value.get_ptr<std::string*>();
+
     if (strPtr == nullptr)
     {
         return ret;
     }
 
-    replaceAll(*strPtr, std::string(templateChar) + "index",
-               std::to_string(index));
-    if (replaceStr)
-    {
-        replaceAll(*strPtr, *replaceStr, std::to_string(index));
-    }
+    std::string str = *strPtr;
 
-    if (templateCharReplaceLoop(strPtr, interface, ret, value))
-    {
-        return ret;
-    }
+    templateCharReplaceStr(str, interface, index, replaceStr);
 
-    strPtr = value.get_ptr<std::string*>();
-    if (strPtr == nullptr)
-    {
-        return ret;
-    }
-
-    const std::optional<uint64_t> optNum = parseAsNumber(*strPtr);
+    const std::optional<uint64_t> optNum = parseAsNumber(str);
     if (optNum.has_value())
     {
         value = optNum.value();
+    }
+    else
+    {
+        value = str;
     }
 
     return ret;

test/entity_manager/test_entity-manager.cpp
@@ -10,6 +10,17 @@
 
 using namespace std::string_literals;
 
+TEST(TemplateCharReplace, replaceIndex)
+{
+    nlohmann::json j = {{"foo", "$index"}};
+    DBusInterface data;
+    data["BUS"] = 23;
+
+    em_utils::templateCharReplace(j, data, 3);
+
+    EXPECT_EQ(3, j["foo"]);
+}
+
 TEST(TemplateCharReplace, replaceOneInt)
 {
     nlohmann::json j = {{"foo", "$bus"}};
@@ -79,6 +90,17 @@ TEST(TemplateCharReplace, increment)
     em_utils::templateCharReplace(j, data, 0);
 
     nlohmann::json expected = "3 plus 1 equals 4";
+    EXPECT_EQ(expected, j["foo"]);
+}
+
+TEST(TemplateCharReplace, incrementIndex)
+{
+    nlohmann::json j = {{"foo", "$index + 3"}};
+    DBusInterface data;
+
+    em_utils::templateCharReplace(j, data, 2);
+
+    nlohmann::json expected = 5;
     EXPECT_EQ(expected, j["foo"]);
 }
 

commit 814a2883047f537d2d197ef02d642346b7baadd1

fix zeroed out fields being recognized as strings
A previous patch caused a regression on qemu catalina-bmc.

Thanks to Rick for reporting and bisecting.

The issue is that zeroed out FRU fields caused `PartNumber` to be seen
as a number instead of a string, but it needs to be a string to match
the interface definition.

Fix this by adding an edge case to the parsing logic for anything
starting with "00".

Alternatives considered:

- introducing a data structure to let EM know the D-Bus data type of the
  property it is exposing. This would allow EM to be a little smarter
  than pure duck-typing and allow it to try and convert to the intended
  type rather than converting purely based on the value.

Fixes: 51b7b9737493 ("extract function templateCharReplaceStr")

Change-Id: Ib69fb0233830645b70cd8ff5660e0c7761ee3e48
Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

src/entity_manager/utils.cpp
@@ -299,11 +299,22 @@ static void templateCharReplaceArray(
 // @returns uint64_t number if it can be parsed as such
 static std::optional<uint64_t> parseAsNumber(std::string_view strView)
 {
+    if (strView.empty()) {
+        return std::nullopt;
+    }
+
     int base = 10;
     if (strView.starts_with("0x"))
     {
         strView.remove_prefix(2);
         base = 16;
+    } else {
+
+        // zeroed out FRU fields are not seen as numbers
+        if (strView.starts_with("00"))
+        {
+            return std::nullopt;
+        }
     }
 
     uint64_t temp = 0;

test/entity_manager/test_entity-manager.cpp
@@ -202,6 +202,17 @@ TEST(TemplateCharReplace, twoReplacementsWithMath2)
     EXPECT_EQ(expected, j["foo"]);
 }
 
+TEST(TemplateCharReplace, hexWithLeadingZeros)
+{
+    nlohmann::json j = {{"foo", "0x0023"}};
+    DBusInterface data;
+
+    em_utils::templateCharReplace(j, data, 0);
+
+    nlohmann::json expected = 0x0023;
+    EXPECT_EQ(expected, j["foo"]);
+}
+
 TEST(TemplateCharReplace, hexAndWrongCase)
 {
     nlohmann::json j = {{"Address", "0x54"},
@@ -244,6 +255,20 @@ TEST(TemplateCharReplace, singleHex)
 
     nlohmann::json expected = 84;
     EXPECT_EQ(expected, j["foo"]);
+}
+
+TEST(TemplateCharReplace, zeroedOutFRUFieldsAreNotNumbers)
+{
+    nlohmann::json j = {{"PartNumber", "$BOARD_PART_NUMBER"}};
+    DBusInterface data;
+
+    // this represents a zeroed out field in a FRU eeprom
+    data["BOARD_PART_NUMBER"] = "00000000000";
+
+    em_utils::templateCharReplace(j, data, 0);
+
+    nlohmann::json expected = "00000000000";
+    EXPECT_EQ(expected, j["PartNumber"]);
 }
 
 TEST(TemplateCharReplace, leftOverTemplateVars)